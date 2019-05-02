95f9585ef0c819c8b22c712f3f532abeb9da88dc
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2019-05-02 13:09:23 -0700

    Implement PubSub for connections

---
+++ server/rpc/pub_sub.go
@@ -0,0 +1,96 @@
+// Copyright (c) 2019 Coinbase, Inc. See LICENSE
+
+package rpc
+
+import (
+	"log"
+	"sync"
+
+	"github.com/pkg/errors"
+)
+
+type connectionSet = map[Connection]struct{}
+
+// PubSub - pub/sub for connections
+type PubSub struct {
+	connMapLock sync.Mutex
+	connMap     map[string]connectionSet
+}
+
+// NewPubSub - construct a PubSub
+func NewPubSub() *PubSub {
+	return &PubSub{
+		connMap: map[string]connectionSet{},
+	}
+}
+
+// Subscribe - subscribes a connection to an id
+func (cm *PubSub) Subscribe(id string, connection Connection) {
+	if id == "" || connection == nil {
+		return
+	}
+	cm.connMapLock.Lock()
+	defer cm.connMapLock.Unlock()
+
+	set, ok := cm.connMap[id]
+	if !ok {
+		set = connectionSet{}
+		cm.connMap[id] = set
+	}
+
+	set[connection] = struct{}{}
+}
+
+// Unsubscribe - unsubscribes a connection from an id
+func (cm *PubSub) Unsubscribe(id string, connection Connection) {
+	if id == "" || connection == nil {
+		return
+	}
+	cm.connMapLock.Lock()
+	defer cm.connMapLock.Unlock()
+
+	set, ok := cm.connMap[id]
+	if !ok {
+		return
+	}
+
+	delete(set, connection)
+
+	if len(set) == 0 {
+		delete(cm.connMap, id)
+	}
+}
+
+// UnsubscribeAll - unsubscribes all connections from an id
+func (cm *PubSub) UnsubscribeAll(id string) {
+	if id == "" {
+		return
+	}
+	cm.connMapLock.Lock()
+	defer cm.connMapLock.Unlock()
+
+	delete(cm.connMap, id)
+}
+
+// Publish - publishes a message to connections that are subscribed to an id
+func (cm *PubSub) Publish(id string, msg interface{}) {
+	if id == "" {
+		return
+	}
+	cm.connMapLock.Lock()
+	defer cm.connMapLock.Unlock()
+
+	connections, ok := cm.connMap[id]
+	if !ok {
+		return
+	}
+
+	for connection := range connections {
+		connection := connection
+		go func() {
+			if err := connection.SendMessage(msg); err != nil {
+				log.Println(errors.Wrap(err, "unable to send message"))
+			}
+		}()
+	}
+}
---
+++ server/rpc/pub_sub_test.go
@@ -0,0 +1,120 @@
+// Copyright (c) 2019 Coinbase, Inc. See LICENSE
+
+package rpc
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mockConn struct {
+	msgSent interface{}
+}
+
+var _ Connection = (*mockConn)(nil)
+
+func (mc *mockConn) HandleMessage(msg *Request) error {
+	return nil
+}
+
+func (mc *mockConn) SendMessage(msg interface{}) error {
+	mc.msgSent = msg
+	return nil
+}
+
+func (mc *mockConn) resetMsgSent() {
+	mc.msgSent = nil
+}
+
+func TestPubSub(t *testing.T) {
+	conn1 := &mockConn{}
+	conn2 := &mockConn{}
+
+	pubsub := NewPubSub()
+
+	// publishing when nothing is subscribed does nothing
+	pubsub.Publish("1", "hello")
+	assert.Equal(t, conn1.msgSent, nil)
+	assert.Equal(t, conn2.msgSent, nil)
+
+	// subscribe conn1 to "1"
+	pubsub.Subscribe("1", conn1)
+
+	// publish "foo" to "1"
+	pubsub.Publish("1", "foo")
+
+	// wait a bit to let async processing finish
+	time.Sleep(5 * time.Millisecond)
+
+	assert.Equal(t, conn1.msgSent, "foo")
+	assert.Equal(t, conn2.msgSent, nil)
+
+	// subscribe conn2 to "1"
+	pubsub.Subscribe("1", conn2)
+
+	conn1.resetMsgSent()
+	conn2.resetMsgSent()
+
+	// publish "bar" to "1"
+	pubsub.Publish("1", "bar")
+
+	time.Sleep(5 * time.Millisecond)
+
+	assert.Equal(t, conn1.msgSent, "bar")
+	assert.Equal(t, conn2.msgSent, "bar")
+
+	conn1.resetMsgSent()
+	conn2.resetMsgSent()
+
+	// subscribe conn1 to "2"
+	pubsub.Subscribe("2", conn1)
+
+	// publish "baz" to "2"
+	pubsub.Publish("2", "baz")
+
+	time.Sleep(5 * time.Millisecond)
+
+	assert.Equal(t, conn1.msgSent, "baz")
+	assert.Equal(t, conn2.msgSent, nil)
+
+	conn1.resetMsgSent()
+	conn2.resetMsgSent()
+
+	// unsubscribe all connections from "2"
+	pubsub.UnsubscribeAll("2")
+
+	// publish "bazz" to "2"
+	pubsub.Publish("2", "bazz")
+
+	time.Sleep(5 * time.Millisecond)
+
+	assert.Equal(t, conn1.msgSent, nil)
+	assert.Equal(t, conn2.msgSent, nil)
+
+	// unsubscribe conn1 from "1"
+	pubsub.Unsubscribe("1", conn1)
+
+	// publish "qux" to "1"
+	pubsub.Publish("1", "qux")
+
+	time.Sleep(5 * time.Millisecond)
+
+	assert.Equal(t, conn1.msgSent, nil)
+	assert.Equal(t, conn2.msgSent, "qux")
+
+	conn1.resetMsgSent()
+	conn2.resetMsgSent()
+
+	// unsubscribe conn2 from "1"
+	pubsub.Unsubscribe("1", conn2)
+
+	// publish "quux" to "1"
+	pubsub.Publish("1", "quux")
+
+	time.Sleep(5 * time.Millisecond)
+
+	assert.Equal(t, conn1.msgSent, nil)
+	assert.Equal(t, conn2.msgSent, nil)
+}
