17b6ab22977eaf1d08c617ac73fc4aa5523d2111
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2019-05-11 18:05:12 -0700

    Bootstrap React web app

---
+++ web/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <p>WalletLink</p>
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import "./index.css"
+// import * as serviceWorker from "./serviceWorker"
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+// serviceWorker.unregister()
