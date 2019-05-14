bd60fb7f128c765c236d9a5f64c9fd35516e1e46
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2019-05-14 15:47:19 -0700

    Implement authentication in WalletLinkHost

--- web/src/WalletLinkHost/WalletLinkHost.ts
+++ web/src/WalletLinkHost/WalletLinkHost.ts
@@ -1,18 +1,50 @@
-import { iif, Subscription, timer } from "rxjs"
-import { delay, filter, flatMap, retry, skip, switchMap } from "rxjs/operators"
+import {
+  BehaviorSubject,
+  iif,
+  Observable,
+  of,
+  race,
+  Subscription,
+  throwError,
+  timer
+} from "rxjs"
+import {
+  delay,
+  distinctUntilChanged,
+  filter,
+  flatMap,
+  map,
+  retry,
+  skip,
+  switchMap
+} from "rxjs/operators"
+import {
+  ClientMessage,
+  ClientMessageHostSession,
+  ServerMessage,
+  ServerMessageFail,
+  ServerMessageOK
+} from "./messages"
 import { ConnectionState, RxWebSocket } from "./RxWebSocket"
 
 const HEARTBEAT_INTERVAL = 10000
+const REQUEST_TIMEOUT = 60000
 
 export class WalletLinkHost {
   private ws: RxWebSocket
   private subscriptions = new Subscription()
   private destroyed = false
-
   private lastHeartbeatResponse = 0
+  private nextReqId = 0
+  private readySubject = new BehaviorSubject(false)
 
-  constructor(url: string, WebSocketClass: typeof WebSocket = WebSocket) {
-    const ws = (this.ws = new RxWebSocket(url, WebSocketClass))
+  constructor(
+    private sessionId: string,
+    private sessionKey: string,
+    rpcUrl: string,
+    WebSocketClass: typeof WebSocket = WebSocket
+  ) {
+    const ws = (this.ws = new RxWebSocket(rpcUrl, WebSocketClass))
 
     // attempt to reconnect every 5 seconds when disconnected
     this.subscriptions.add(
@@ -26,6 +58,25 @@ export class WalletLinkHost {
           retry()
         )
         .subscribe()
+    )
+
+    // perform authentication upon connection
+    this.subscriptions.add(
+      ws.connectionState$
+        .pipe(
+          // skip initial DISCONNECTED and CONNECTING
+          skip(2),
+          // if connected, attempt to authenticate, else emit false
+          switchMap(cs =>
+            iif(
+              () => cs === ConnectionState.CONNECTED,
+              this.authenticate(),
+              of(false)
+            )
+          ),
+          distinctUntilChanged()
+        )
+        .subscribe(isReady => this.readySubject.next(isReady))
     )
 
     // send heartbeat every n seconds while connected
@@ -66,6 +117,13 @@ export class WalletLinkHost {
     this.destroyed = true
   }
 
+  /**
+   * emits true if connected and authenticated, else false
+   */
+  public get ready$(): Observable<boolean> {
+    return this.readySubject.asObservable()
+  }
+
   private updateLastHeartbeat(): void {
     this.lastHeartbeatResponse = Date.now()
   }
@@ -78,5 +136,36 @@ export class WalletLinkHost {
     try {
       this.ws.sendData("h")
     } catch {}
+  }
+
+  private makeRequest<T extends ServerMessage>(
+    message: ClientMessage,
+    timeout: number = REQUEST_TIMEOUT
+  ): Observable<T> {
+    const reqId = message.id
+    try {
+      this.ws.sendData(JSON.stringify(message))
+    } catch (err) {
+      return throwError(err)
+    }
+    return race(
+      this.ws.incomingJSONData$.pipe(
+        filter((m: ServerMessage) => m.id === reqId)
+      ),
+      timer(timeout).pipe(
+        flatMap(_ => throwError(`request ${reqId} timed out`))
+      )
+    )
+  }
+
+  private authenticate(): Observable<boolean> {
+    const hostSession = ClientMessageHostSession(
+      this.nextReqId++,
+      this.sessionId,
+      this.sessionKey
+    )
+    return this.makeRequest<ServerMessageOK | ServerMessageFail>(
+      hostSession
+    ).pipe(map(res => res.type === "OK"))
   }
 }
