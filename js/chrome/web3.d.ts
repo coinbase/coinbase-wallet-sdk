d902b7125ea961684ca1e75048c3826f488e6a35
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2019-05-17 13:14:33 -0700

    Implement WalletLinkProvider

---
+++ js/chrome/web3.d.ts
@@ -0,0 +1,3 @@
+declare module "web3" {
+  export default class Web3 {}
+}
--- js/src/WalletLink.ts
+++ js/src/WalletLink.ts
@@ -1,6 +1,27 @@
-/**
- * @copyright Copyright (c) 2018-2019 Coinbase, Inc.
- * @license Apache License, version 2.0
- */
+import { WalletLinkProvider } from "./WalletLinkProvider"
 
-export class WalletLink {}
+// Copyright (c) 2018-2019 Coinbase, Inc. <https://coinbase.com/>
+// Licensed under the Apache License, version 2.0
+
+export interface WalletLinkOptions {
+  appName?: string
+}
+
+export class WalletLink {
+  private _appName?: string
+
+  constructor(options: WalletLinkOptions) {
+    this._appName = options.appName || "DApp"
+  }
+
+  public makeWeb3Provider(
+    jsonRpcUrl: string,
+    chainId: number = 1
+  ): WalletLinkProvider {
+    return new WalletLinkProvider({
+      appName: this._appName,
+      jsonRpcUrl,
+      chainId
+    })
+  }
+}
