16ffaac3afae7fe18aa434881b4b3090b174af83
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2019-06-06 17:09:16 -0700

    Index keys in the postgres store

--- schema.sql
+++ schema.sql
@@ -1,6 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
 CREATE TABLE store (
   key         varchar(256) PRIMARY KEY,
   value       text NOT NULL,
   created_at  timestamptz NOT NULL DEFAULT now(),
   updated_at  timestamptz NOT NULL DEFAULT now()
 );
+
+CREATE INDEX trgm_idx_store_key ON store USING gin (key gin_trgm_ops);
