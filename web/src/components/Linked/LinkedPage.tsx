62f913561f671b89eee32d351e1207a82af1f090
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2019-08-22 12:03:36 -0700

    Update copy on Linked page

--- web/src/components/Linked/LinkedPage.tsx
+++ web/src/components/Linked/LinkedPage.tsx
@@ -32,8 +32,8 @@ export class LinkedPage extends React.PureComponent {
         <h3 className={styles.title}>Authorize on Wallet App</h3>
 
         <p className={styles.para}>
-          A request to connect your wallet to this app has been pushed to your
-          phone. Check your wallet app to continue.
+          We&rsquo;ve sent a request to your mobile device. Check your Wallet
+          app to continue.
         </p>
       </PopUpFrame>
     )
