883314e1ed2f0c1425377a71d949b80077a027c5
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2020-01-29 16:49:03 -0800

    Set up scss compilation

---
+++ js/compile-scss.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2018-2020 Coinbase, Inc. <https://coinbase.com/>
+// Licensed under the Apache License, version 2.0
+
+const fs = require("fs")
+const glob = require("glob")
+const sass = require("sass")
+
+glob(`${__dirname}/src/**/*.scss`, (_, matches) => {
+  matches.forEach(filePath => {
+    console.log(`Compiling ${filePath}...`)
+    const css = sass
+      .renderSync({ file: filePath, outputStyle: "compressed" })
+      .css.toString("utf8")
+    const ts = `// Copyright (c) 2018-2020 Coinbase, Inc. <https://coinbase.com/>
+// Licensed under the Apache License, version 2.0
+
+export default \`${css}\`
+`
+    fs.writeFileSync(filePath.replace(/\.scss$/, ".css.ts"), ts, {
+      mode: 0o644
+    })
+  })
+  console.log("DONE!")
+})
