affeaf3b9f3d9b920e697da7689d9cd2e95922a0
Peter Jihoon Kim <petejkim@users.noreply.github.com>
2020-01-29 16:49:30 -0800

    Set up Preact

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: {
           loader: "ts-loader",
           options: {
@@ -33,7 +33,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: [".ts", ".tsx", ".js"],
     plugins: [],
     symlinks: false
   },
