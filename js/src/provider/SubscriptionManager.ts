b426926c5e3985801f3765677fcb76f5729a8d76
Vishnu Madhusoodanan <vishnu.madhusoodanan@coinbase.com>
2021-03-01 12:09:06 -0600

    Add type for middleware

--- js/src/provider/SubscriptionManager.ts
+++ js/src/provider/SubscriptionManager.ts
@@ -1,36 +1,56 @@
 import SafeEventEmitter from "@metamask/safe-event-emitter"
-import { Web3Provider } from "./Web3Provider"
+import { RequestArguments, Web3Provider } from "./Web3Provider"
+import {
+  JsonRpcEngineNextCallback,
+  JsonRpcEngineEndCallback
+} from "json-rpc-engine"
 const PollingBlockTracker = require("eth-block-tracker")
 const createSubscriptionManager = require("eth-json-rpc-filters/subscriptionManager")
 const noop = () => {}
 
 export class SubscriptionManager {
-  private readonly subscriptionMiddleware: any
+  private readonly subscriptionMiddleware: SubscriptionMiddleware
   readonly events: SafeEventEmitter
 
   constructor(provider: Web3Provider) {
     const blockTracker = new PollingBlockTracker({
-      provider: provider,
+      provider,
       pollingInterval: 15 * 1000, // 15 sec
       setSkipCacheFlag: true
     })
 
     const { events, middleware } = createSubscriptionManager({
-      blockTracker: blockTracker,
-      provider: provider
+      blockTracker,
+      provider
     })
 
     this.events = events
     this.subscriptionMiddleware = middleware
   }
 
-  public async handleRequest(request: { method: string; params: any[] }) {
-    let result = {}
-    await this.subscriptionMiddleware(request, result, {}, noop)
+  public async handleRequest(request: {
+    method: string
+    params: any[]
+  }): Promise<SubscriptionResult> {
+    const result = {}
+    await this.subscriptionMiddleware(request, result, noop, noop)
     return result
   }
 
   public destroy() {
     this.subscriptionMiddleware.destroy()
   }
+}
+
+type SubscriptionResult = { result?: any }
+
+interface SubscriptionMiddleware {
+  (
+    req: RequestArguments,
+    res: SubscriptionResult,
+    next: JsonRpcEngineNextCallback,
+    end: JsonRpcEngineEndCallback
+  ): Promise<void>
+
+  destroy(): void
 }
