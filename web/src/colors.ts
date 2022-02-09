ff5cd32e2fb3c7e887171557767ac3c85c0ac805
Jungho Bang <jungho.bang@coinbase.com>
2022-02-09 14:47:37 -0800

    Delete outdated WL server and webpage source code

--- web/src/colors.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018-2020 WalletLink.org <https://www.walletlink.org/>
-// Copyright (c) 2018-2020 Coinbase, Inc. <https://www.coinbase.com/>
-// Licensed under the Apache License, version 2.0
-
-export const colors = {
-  primary: "#1652f0",
-  background: "#f5f7f8",
-  foreground: "#050f19"
-}
