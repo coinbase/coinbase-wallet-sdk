a0a535bf36551360da5d9f6252b6c478e7139034
Jungho Bang <jungho.bang@coinbase.com>
2022-02-28 14:31:40 -0800

    indent

--- src/connection/EventListener.ts
+++ src/connection/EventListener.ts
@@ -3,8 +3,10 @@ export interface EventListener {
 }
 
 export const EVENTS = {
-  REQUEST_CHILD_ETHEREUM_ACCOUNTS_START: "walletlink_sdk.request_child_ethereum_accounts.start",
-  REQUEST_CHILD_ETHEREUM_ACCOUNTS_RESPONSE: "walletlink_sdk.request_child_ethereum_accounts.response",
+  REQUEST_CHILD_ETHEREUM_ACCOUNTS_START:
+    "walletlink_sdk.request_child_ethereum_accounts.start",
+  REQUEST_CHILD_ETHEREUM_ACCOUNTS_RESPONSE:
+    "walletlink_sdk.request_child_ethereum_accounts.response",
   STARTED_CONNECTING: "walletlink_sdk.started.connecting",
   CONNECTED_STATE_CHANGE: "walletlink_sdk.connected",
   DISCONNECTED: "walletlink_sdk.disconnected",
