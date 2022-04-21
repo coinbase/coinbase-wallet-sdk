e69ac43db459f4d2d210c71caaef8f4f9c661eda
Erin Lee <erin.lee@coinbase.com>
2022-04-21 14:38:38 -0400

    Feedback

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-env", { targets: { node: "10" } }],
     "@babel/preset-typescript",
     "preact"
   ],
+  // TODO: Refactor from legacy decorators
   plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]]
 };
