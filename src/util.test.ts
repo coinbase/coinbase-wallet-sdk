9840903d80eae85fefbe7407aedf172daaa57221
Erin Lee <erin.lee@coinbase.com>
2022-04-25 14:39:38 -0400

    Update util.test.ts

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,5 +1,13 @@
-import { uint8ArrayToHex } from "./util";
+import { hexStringToUint8Array, uint8ArrayToHex } from "./util";
+
+const uint8ArrVal = new Uint8Array(6);
 
 test("uint8ArrayToHex", () => {
-  expect(uint8ArrayToHex(new Uint8Array(6))).toEqual("000000000000");
+  expect(uint8ArrayToHex(uint8ArrVal)).toEqual("000000000000");
+});
+
+test("hexStringToUint8Array", () => {
+  expect(hexStringToUint8Array("9298119f5025")).toEqual(
+    new Uint8Array([146, 152, 17, 159, 80, 37])
+  );
 });
