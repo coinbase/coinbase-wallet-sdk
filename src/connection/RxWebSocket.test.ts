8efee567bdea0243268355125c4e858919265f91
Erin Lee <erin.lee@coinbase.com>
2022-05-11 17:03:38 -0400

    With jest-websocket-mock

--- src/connection/RxWebSocket.test.ts
+++ src/connection/RxWebSocket.test.ts
@@ -1,46 +1,54 @@
+import WS from "jest-websocket-mock";
 import { Observable } from "rxjs";
-import { EventEmitter } from "stream";
 
 import { ConnectionState, RxWebSocket } from "./RxWebSocket";
 
-class MockWebSocket extends EventEmitter {
-  readonly url: string | URL;
-  readonly protocols: string | string[] | undefined;
-  constructor(url: string | URL) {
-    super();
-    this.url = url;
-  }
-  send(data: string) {
-    return data;
-  }
-  close() {
-    this.emit("close");
-  }
-}
-
 describe("RxWebSocket", () => {
-  const rxWS = new RxWebSocket("http://link-url.com");
+  const server = new WS("ws://localhost:1234");
+  const rxWS = new RxWebSocket("http://localhost:1234");
 
-  test("@connect & @disconnect", async () => {
-    const rxWsRes = await rxWS.connect();
+  test.only("@connect & @disconnect", async () => {
+    const client = await rxWS.connect();
 
-    expect(rxWsRes).toBeInstanceOf(Observable);
-    expect(rxWsRes.toPromise()).toBeTruthy();
+    expect(client).toBeInstanceOf(Observable);
+    await client.toPromise();
+    await server.connected; // wait for the server to have established the connection
 
+    // the mock websocket server will record all the messages it receives
+    // rxWS.sendData("hello");
+
+    // the mock websocket server can also send messages to all connected clients
+    // server.send("hello everyone");
     // @ts-expect-error test private methods
     expect(rxWS.webSocket).toBeInstanceOf(WebSocket);
+    // @ts-expect-error test private methods
+    rxWS.connectionStateSubject.subscribe({
+      next: val => {
+        console.log(val);
+
+        expect(val).toEqual(ConnectionState.CONNECTED);
+      },
+    });
 
     await rxWS.disconnect();
     // @ts-expect-error test private methods
     expect(rxWS.webSocket).toBe(null);
+    // @ts-expect-error test private methods
+    rxWS.connectionStateSubject.subscribe({
+      next: val => {
+        console.log(val);
+
+        expect(val).toEqual(ConnectionState.DISCONNECTED);
+      },
+    });
   });
 
-  test("@sendData", () => {
+  test("@sendData", async () => {
     const webSocketSendMock = jest
       .spyOn(WebSocket.prototype, "send")
       .mockImplementation(() => "send");
 
-    expect(rxWS.connect().toPromise()).toBeTruthy();
+    await rxWS.connect().toPromise();
     rxWS.sendData("data");
     expect(webSocketSendMock).toHaveBeenCalledWith("data");
   });
