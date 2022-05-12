cd6aa9b47ac176d154a4fe5d5e17496f48a70938
Erin Lee <erin.lee@coinbase.com>
2022-05-12 16:39:27 -0400

    Create SubscriptionManager.test.ts

---
+++ src/provider/SubscriptionManager.test.ts
@@ -0,0 +1,6 @@
+import { mockExtensionProvider } from "../__mocks__/provider";
+import { SubscriptionManager } from "./SubscriptionManager";
+
+describe("SubscriptionManager", () => {
+  const subscription = new SubscriptionManager(mockExtensionProvider);
+});
