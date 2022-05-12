c1e6e053459b7e8498e888d934ab287c748a4659
Erin Lee <erin.lee@coinbase.com>
2022-05-12 16:48:17 -0400

    Update SubscriptionManager.test.ts

--- src/provider/SubscriptionManager.test.ts
+++ src/provider/SubscriptionManager.test.ts
@@ -1,6 +1,18 @@
 import { mockExtensionProvider } from "../__mocks__/provider";
+import { MOCK_ADDERESS } from "../fixtures/provider";
+import { Web3Method } from "../relay/Web3Method";
+import { AddressString } from "../types";
 import { SubscriptionManager } from "./SubscriptionManager";
 
 describe("SubscriptionManager", () => {
   const subscription = new SubscriptionManager(mockExtensionProvider);
+
+  test("@handleRequest", async () => {
+    await expect(
+      subscription.handleRequest({
+        method: Web3Method.requestEthereumAccounts,
+        params: [AddressString(MOCK_ADDERESS)],
+      }),
+    ).resolves.toStrictEqual({});
+  });
 });
