59f7a4128110347b5c8a2c2965c74b2510169bbf
Jungho Bang <jungho.bang@coinbase.com>
2022-05-19 16:51:37 -0700

    ios example app scaffolding

---
+++ examples/client/ios/ClientApp/ClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientAppApp.swift
+//  ClientApp
+//
+//  Created by Jungho Bang on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ examples/client/ios/ClientApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClientApp
+//
+//  Created by Jungho Bang on 5/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
