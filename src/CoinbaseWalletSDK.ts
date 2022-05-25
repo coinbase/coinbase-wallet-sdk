1e62f0d3ecf993bf3890e9a6c4e223cd21051cd0
JM Cho <jm.cho@coinbase.com>
2022-05-24 20:45:16 -0400

    FIXED: import sorting

--- src/CoinbaseWalletSDK.ts
+++ src/CoinbaseWalletSDK.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2018-2022 Coinbase, Inc. <https://www.coinbase.com/>
 // Licensed under the Apache License, version 2.0
 
-import { walletLogo, LogoType } from "./assets/wallet-logo";
+import { LogoType, walletLogo } from "./assets/wallet-logo";
 import { DiagnosticLogger } from "./connection/DiagnosticLogger";
 import { EventListener } from "./connection/EventListener";
 import { ScopedLocalStorage } from "./lib/ScopedLocalStorage";
