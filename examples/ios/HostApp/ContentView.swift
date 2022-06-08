aa2057b4916b54f2b3e6e47f4f634c6ffa8101bc
Jungho Bang <jungho.bang@coinbase.com>
2022-06-08 16:39:03 -0700

    host example app

---
+++ examples/ios/HostApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HostApp
+//
+//  Created by Jungho Bang on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ examples/ios/HostApp/HostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostAppApp.swift
+//  HostApp
+//
+//  Created by Jungho Bang on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
