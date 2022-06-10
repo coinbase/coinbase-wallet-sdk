1b95572b0dcd78b288a50a034573b94677bbb1db
Taylor Caldwell <taylor.caldwell@coinbase.com>
2022-06-10 00:23:17 -0700

    Add wagmi demo

--- examples/README.md
+++ examples/README.md
@@ -1,6 +1,13 @@
-Coinbase Wallet Examples
---
+## Coinbase Wallet Examples
 
 ### Verify Web App
+
 [Verify Web App Example](verify-webapp/)
+
 - Related documentation: [Deep Linking into the Coinbase Wallet Dapp Browser](https://docs.cloud.coinbase.com/wallet-sdk/docs/deep-link-into-dapp-browser)
+
+### Integrate with wagmi
+
+[wagmi example](wagmi-demo/)
+
+- Related documentation: [Integrating with wagmi](https://docs.cloud.coinbase.com/wallet-sdk/docs/wagmi)
---
+++ examples/wagmi-demo/src/connectors.js
@@ -0,0 +1,38 @@
+import { configureChains, defaultChains } from "wagmi";
+
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+
+// API key for Ethereum node
+// Two popular services are Infura (infura.io) and Alchemy (alchemy.com)
+const infuraId = process.env.INFURA_ID;
+
+// Configure chains for connectors to support
+const { chains } = configureChains(defaultChains, [
+  infuraProvider({ infuraId }),
+  publicProvider(),
+]);
+
+// Set up connectors
+export const connectors = [
+  new CoinbaseWalletConnector({
+    chains,
+    options: {
+      appName: "wagmi demo",
+    },
+  }),
+  new WalletConnectConnector({
+    chains,
+    options: {
+      infuraId,
+      qrcode: true,
+    },
+  }),
+  new MetaMaskConnector({
+    chains,
+  }),
+];
