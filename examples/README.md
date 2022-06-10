d4b31a26693c4491ce41430e2290b09d4bcb6d68
Taylor Caldwell <taylor.caldwell@coinbase.com>
2022-06-10 00:15:24 -0700

    Add web3-react demo

--- examples/README.md
+++ examples/README.md
@@ -1,6 +1,13 @@
-Coinbase Wallet Examples
---
+## Coinbase Wallet Examples
 
 ### Verify Web App
+
 [Verify Web App Example](verify-webapp/)
+
 - Related documentation: [Deep Linking into the Coinbase Wallet Dapp Browser](https://docs.cloud.coinbase.com/wallet-sdk/docs/deep-link-into-dapp-browser)
+
+### Integrate with web3-react
+
+[web3-react example](web3-react-demo/)
+
+- Related documentation: [Integrating with Web3-React](https://docs.cloud.coinbase.com/wallet-sdk/docs/web3-react)
---
+++ examples/web3-react-demo/src/connectors.js
@@ -0,0 +1,24 @@
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+
+const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+const walletconnect = new WalletConnectConnector({
+  rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+  bridge: "https://bridge.walletconnect.org",
+  qrcode: true,
+});
+
+const walletlink = new WalletLinkConnector({
+  url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+  appName: "web3-react-demo",
+});
+
+export const connectors = {
+  injected: injected,
+  walletConnect: walletconnect,
+  coinbaseWallet: walletlink,
+};
