a82d676ad1c71b264fed5d0c2b1217dbb0c6f06b
Jungho Bang <jungho.bang@coinbase.com>
2022-06-27 16:34:20 -0700

    update example app

---
+++ examples/native-sdk-ios-client/NativeWeb3App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativeWeb3App
+//
+//  Created by Jungho Bang on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
