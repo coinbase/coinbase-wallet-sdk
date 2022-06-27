eda648670f95bb89fc4b9d056249f0bad70ec068
Jungho Bang <jungho.bang@coinbase.com>
2022-06-27 16:10:01 -0700

    sample ios client app

---
+++ examples/native-sdk-ios-client/ClientApp/ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  ClientApp
+//
+//  Created by Jungho Bang on 5/19/22.
+//
+
+import SwiftUI
+import CoinbaseWalletSDK
+
+import CryptoKit
+
+struct ContentView: View {
+    let sdk = CoinbaseWalletSDK(callback: URL(string: "https://go.cb-w.com/")!)
+    
+    let clientPrivateKey = Curve25519.KeyAgreement.PrivateKey()
+//    @State var request: String?
+    
+    @State var response: String?
+    
+    @State var parsedResponse: Data?
+    @State var decryptedMessage: String?
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text("Generated Private Key: ")
+                Text(String(data: self.clientPrivateKey.rawRepresentation, encoding: .ascii) ?? "(none)")
+                
+                Button("Initiate handshake request") {
+                    sdk.initiateHandshake{ result in
+                        print(result)
+                    }
+                }
+//                HStack {
+//                    Text("Request: ")
+//                    Text(self.request ?? "(none)")
+//                }
+            }
+            
+            Spacer()
+            
+            VStack {
+                Button("(Host) Handle handshake request") {
+//                    let handler = HandshakeRequestHandler()
+//                    let response = handler.handle(request: self.request!)
+//                    self.response = try! response.encodedString()
+                }
+                HStack {
+                    Text("Response: ")
+                    Text(self.response ?? "(none)")
+                }
+            }
+            
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Button("Parse handshake response from Host") {
+//                    self.parsedResponse = try! HandshakeResponse.decode(self.response!)
+                }
+                HStack {
+                    Text("Message: ")
+//                    Text(String(data: self.parsedResponse?.message ?? Data(), encoding: .ascii) ?? "(none)")
+                }
+                HStack {
+                    Text("Host Pub Key: ")
+//                    Text(String(data: self.parsedResponse?.hostPublicKey.rawRepresentation ?? Data(), encoding: .ascii) ?? "(none)")
+                }
+            }
+            
+            Spacer()
+            
+            Button("Decrypt") {
+                // client derives symmetric key
+//                let symmetricKey = sdk.deriveSymmetricKey(with: self.clientPrivateKey, self.parsedResponse!.hostPublicKey)
+//                
+//                let sealedBox = try! AES.GCM.SealedBox(combined: self.parsedResponse!.message)
+//                let decryptedData = try! AES.GCM.open(sealedBox, using: symmetricKey)
+//                self.decryptedMessage = String(data: decryptedData, encoding: .utf8)
+            }
+            Text(self.decryptedMessage ?? "(none)")
+                .lineLimit(nil)
+            
+            Spacer()
+        }.lineLimit(2)
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ examples/native-sdk-ios-client/README.md
@@ -0,0 +1,1 @@
+#  Sample client iOS app
