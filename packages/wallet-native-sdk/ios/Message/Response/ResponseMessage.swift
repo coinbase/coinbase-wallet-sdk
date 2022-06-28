1b89d7554119fa31ba8587a5ba9bae711f5eb5f8
Jungho Bang <jungho.bang@coinbase.com>
2022-06-27 17:00:38 -0700

    define ReturnType

--- packages/wallet-native-sdk/ios/Message/Response/ResponseMessage.swift
+++ packages/wallet-native-sdk/ios/Message/Response/ResponseMessage.swift
@@ -10,7 +10,7 @@ import Foundation
 @available(iOS 13.0, *)
 public enum ResponseContent: UnencryptedContent {
     case response(requestId: UUID, values: [ReturnValue])
-    case error(requestId: UUID, description: String)
+    case failure(requestId: UUID, description: String)
 }
 
 @available(iOS 13.0, *)
