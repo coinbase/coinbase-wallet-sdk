2947690ec23c95bccb30147a12f6a91430aca0fb
Jungho Bang <jungho.bang@coinbase.com>
2022-06-30 12:55:24 -0700

    remove old files

--- examples/ios/Pods/Target Support Files/NativeSDKHost/NativeSDKHost-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NativeSDKHost : NSObject
-@end
-@implementation PodsDummy_NativeSDKHost
-@end
--- packages/wallet-native-sdk/android/CoinbaseWalletNativeSDK/src/main/java/com/coinbase/android/nativesdk/CoinbaseWalletSDKError.kt
+++
@@ -1,9 +0,0 @@
-package com.coinbase.android.nativesdk
-
-sealed class CoinbaseWalletSDKError(errorMessage: String? = null) : Exception(errorMessage) {
-    object EncodingFailed : CoinbaseWalletSDKError("Encoding failed")
-    object DecodingFailed : CoinbaseWalletSDKError("Decoding failed")
-    object MissingSharedSecret : CoinbaseWalletSDKError("Missing shared secret")
-    object OpenWalletFailed : CoinbaseWalletSDKError("Could not open wallet")
-    class WalletReturnedError(error: String) : CoinbaseWalletSDKError(error)
-}
