c04147be8910f1f6c732c0d2a5f33fb3daadc9be
Amit Goel <amit.nyc@gmail.com>
2022-07-18 16:31:02 -0400

    Android WalletSDK Native Demo App improvements (#598)
    * Now displays connected state
    * ActionsManager to use mocked actions to send in either handshake or request
    * Persists eth account info request processing
    
    Co-authored-by: Amit Goel <amit.goel@coinbase.com>

--- examples/native-sdk-android-client/app/src/main/java/com/coinbase/android/beta/MainActivity.kt
+++ examples/native-sdk-android-client/app/src/main/java/com/coinbase/android/beta/MainActivity.kt
@@ -6,12 +6,16 @@ import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.isVisible
 import com.coinbase.android.beta.databinding.ActivityMainBinding
 import com.coinbase.android.nativesdk.CoinbaseWalletSDK
+import com.coinbase.android.nativesdk.message.request.Action
 import com.coinbase.android.nativesdk.message.request.RequestContent
-import com.coinbase.android.nativesdk.message.request.Web3JsonRPC
 import com.coinbase.android.nativesdk.message.response.ReturnValue
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        SharedPrefsManager.init(this)
         launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             val uri = result.data?.data ?: return@registerForActivityResult
             client.handleResponse(uri)
@@ -42,48 +47,28 @@ class MainActivity : AppCompatActivity() {
 
         setVisibility()
         connectWalletButton.setOnClickListener {
-            client.initiateHandshake(
-                initialActions = listOf(
-                    Web3JsonRPC.RequestAccounts().action(), //Web3JsonRPC.SwitchEthereumChain(420).action()
-                )
-            ) { result: Result<List<ReturnValue>> ->
-                result
-                    .onSuccess { returnValues: List<ReturnValue> ->
-                        textArea.text = buildString {
-                            append("Handshake Successful\n\n")
-                            returnValues.forEach { returnValue ->
-                                if (returnValue is ReturnValue.Result) {
-                                    append("Result: ${returnValue.value}")
-                                }
-                            }
-                        }
-                        setVisibility()
-                    }
-                    .onFailure { err ->
-                        textArea.text = buildString {
-                            append("Handshake Error: \n\n")
-                            append(err.message)
-                        }
-                    }
+            val handShakeActions = ActionsManager.handShakeActions
+            client.initiateHandshake(initialActions = handShakeActions) { result: Result<List<ReturnValue>> ->
+                result.onSuccess { returnValues: List<ReturnValue> ->
+                    returnValues.handleSuccess("Handshake", handShakeActions)
+                }
+                result.onFailure { err ->
+                    err.handleError("HandShake")
+                }
             }
         }
 
         personalSign.setOnClickListener {
-            client.makeRequest(
-                request = RequestContent.Request(
-                    actions = listOf(
-                        //                        Web3JsonRPC.SwitchEthereumChain(420).action(),
-                        //                        Web3JsonRPC.RequestAccounts().action(),
-                        Web3JsonRPC.PersonalSign("0xabcdefabcdefabcdefabcdefabcdefabcdef", "Hello world").action()
-                    )
-                )
-            ) { result ->
+            val requestActions = ActionsManager.getRequestActions(
+                toAddress = "0x571a6a108adb08f9ca54fe8605280f9ee0ed4af6"
+            )
+            client.makeRequest(request = RequestContent.Request(actions = requestActions)) { result ->
                 result.fold(
-                    onSuccess = { res ->
-                        //                        textArea.text = "Request Successful: \n${res.results.joinToString()}"
+                    onSuccess = { returnValues ->
+                        returnValues.handleSuccess("Request", requestActions)
                     },
                     onFailure = { err ->
-                        textArea.text = "Request Error: \n${err.message}"
+                        err.handleError("Request")
                     }
                 )
             }
@@ -91,7 +76,43 @@ class MainActivity : AppCompatActivity() {
 
         removeAccount.setOnClickListener {
             client.resetSession()
+            SharedPrefsManager.removeAccount()
+            textArea.setText(R.string.connection_removed)
             setVisibility()
+        }
+    }
+
+    private fun List<ReturnValue>.handleSuccess(requestType: String, actions: List<Action>) = with(binding) {
+        textArea.text = buildString {
+            if (actions.isEmpty()) {
+                append("Handshake successful")
+            } else {
+                this@handleSuccess.forEachIndexed { index, returnValue ->
+                    append(
+                        "${actions[index].method} Result: " +
+                                "${if (returnValue is ReturnValue.Result) "Success" else "Error"}\n"
+                    )
+
+                    val result = when (returnValue) {
+                        is ReturnValue.Result -> {
+                            if (actions[index].method == "eth_requestAccounts") {
+                                SharedPrefsManager.account = returnValue.value
+                            }
+                            returnValue.value
+                        }
+                        is ReturnValue.Error -> returnValue.message
+                    }
+                    append("${result}\n\n")
+                }
+            }
+        }
+        setVisibility()
+    }
+
+    private fun Throwable.handleError(requestType: String) = with(binding) {
+        textArea.text = buildString {
+            append("$requestType Error: \n\n")
+            append(message)
         }
     }
 
@@ -99,5 +120,31 @@ class MainActivity : AppCompatActivity() {
         val isConnected = client.hasEstablishedConnection
         connectContainer.isVisible = !isConnected
         requestContainer.isVisible = isConnected
+
+
+        with(connectedStatus) {
+            val radius = 48f
+            val shapeAppearanceModel = ShapeAppearanceModel()
+                .toBuilder()
+                .setAllCorners(CornerFamily.ROUNDED, radius)
+                .build()
+
+            val shapeDrawable = MaterialShapeDrawable(shapeAppearanceModel)
+            ViewCompat.setBackground(connectedStatus, shapeDrawable)
+
+            if (isConnected) {
+                text = getString(
+                    R.string.connected_state,
+                    "${SharedPrefsManager.account.take(6)}...${SharedPrefsManager.account.takeLast(4)}"
+                )
+                setTextColor(getColor(R.color.teal_200))
+                shapeDrawable.setStroke(2.0f, getColor(R.color.teal_200));
+
+            } else {
+                text = getString(R.string.unconnected_state)
+                setTextColor(getColor(R.color.red_error))
+                shapeDrawable.setStroke(2.0f, getColor(R.color.red_error));
+            }
+        }
     }
 }
