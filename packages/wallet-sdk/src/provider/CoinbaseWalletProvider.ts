abb1d79479922127c7c7e7980941e053f6ec7da0
Jake Panitz <jake.panitz@gmail.com>
2022-08-16 19:52:26 -0400

    lint

--- packages/wallet-sdk/src/provider/CoinbaseWalletProvider.ts
+++ packages/wallet-sdk/src/provider/CoinbaseWalletProvider.ts
@@ -187,7 +187,7 @@ export class CoinbaseWalletProvider
         const jsonRpcUrl = event.data.data.jsonRpcUrl ?? this.jsonRpcUrl;
         this.updateProviderInfo(jsonRpcUrl, Number(_chainId), true);
       }
-      
+
       if (event.data.data.action === "addressChanged") {
         this._setAddresses([event.data.data.address]);
       }
