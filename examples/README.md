d91775cbc0f0cbc370fb36ca408e07a1a72e6ca5
taycaldwell <taycaldwell@users.noreply.github.com>
2023-01-09 18:56:45 -0700

    Add web3modal demo (#568)

--- examples/README.md
+++ examples/README.md
@@ -6,6 +6,12 @@
 
 - Related documentation: [Deep Linking into the Coinbase Wallet Dapp Browser](https://docs.cloud.coinbase.com/wallet-sdk/docs/deep-link-into-dapp-browser)
 
+### Integrate with web3modal
+
+[web3modal example](web3modal-demo/)
+
+- Related documentation: [Integrating with Web3Modal](https://docs.cloud.coinbase.com/wallet-sdk/docs/web3modal)
+
 ### Integrate with web3-onboard
 
 [web3-onboard example](web3-onboard-demo/)
---
+++ examples/web3modal-demo/src/providerOptions.js
@@ -0,0 +1,18 @@
+import WalletConnect from "@walletconnect/web3-provider";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+
+export const providerOptions = {
+  walletlink: {
+    package: CoinbaseWalletSDK, // Required
+    options: {
+      appName: "Web3Modal Demo", // Required
+      infuraId: process.env.INFURA_ID, // Required unless you provide a JSON RPC url; see `rpc` below
+    },
+  },
+  walletconnect: {
+    package: WalletConnect, // required
+    options: {
+      infuraId: process.env.INFURA_ID, // required
+    },
+  },
+};
