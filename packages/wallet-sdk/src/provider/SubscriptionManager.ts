d3030057b7b645cce0ded04c4022f66224c70010
Jungho Bang <jungho.bang@coinbase.com>
2023-06-30 15:57:32 -0700

    Revert "for testing"
    This reverts commit fec69baf83249959a6fc3d8da406dc094bbae4f6.

--- packages/wallet-sdk/src/provider/SubscriptionManager.ts
+++ packages/wallet-sdk/src/provider/SubscriptionManager.ts
@@ -1,9 +1,14 @@
 import SafeEventEmitter from "@metamask/safe-event-emitter";
+import { PollingBlockTracker, Provider } from "eth-block-tracker";
+import {
+  JsonRpcEngineEndCallback,
+  JsonRpcEngineNextCallback,
+} from "json-rpc-engine";
 
-import { Web3Provider } from "./Web3Provider";
+import { RequestArguments, Web3Provider } from "./Web3Provider";
 
-// const createSubscriptionManager = require("eth-json-rpc-filters/subscriptionManager");
-// const noop = () => {};
+const createSubscriptionManager = require("eth-json-rpc-filters/subscriptionManager");
+const noop = () => {};
 
 export interface SubscriptionResult {
   result?: unknown;
@@ -18,47 +23,46 @@ export interface SubscriptionNotification {
 }
 
 export class SubscriptionManager {
-  // private readonly subscriptionMiddleware: SubscriptionMiddleware;
-  // readonly events: SafeEventEmitter;
+  private readonly subscriptionMiddleware: SubscriptionMiddleware;
+  readonly events: SafeEventEmitter;
 
-  constructor(_: Web3Provider & SafeEventEmitter) {
-    // const blockTracker = new PollingBlockTracker({
-    //   provider: provider as any,
-    //   pollingInterval: 15 * 1000, // 15 sec
-    //   setSkipCacheFlag: true,
-    // });
+  constructor(provider: Web3Provider & SafeEventEmitter) {
+    const blockTracker = new PollingBlockTracker({
+      provider: provider as Provider,
+      pollingInterval: 15 * 1000, // 15 sec
+      setSkipCacheFlag: true,
+    });
 
-    // const { events, middleware } = createSubscriptionManager({
-    //   blockTracker,
-    //   provider,
-    // });
+    const { events, middleware } = createSubscriptionManager({
+      blockTracker,
+      provider,
+    });
 
-    // this.events = events;
-    // this.subscriptionMiddleware = middleware;
+    this.events = events;
+    this.subscriptionMiddleware = middleware;
   }
 
-  public async handleRequest(_: {
+  public async handleRequest(request: {
     method: string;
     params: any[];
   }): Promise<SubscriptionResult> {
     const result = {};
-    await Promise.resolve();
-    // await this.subscriptionMiddleware(request, result, noop, noop);
+    await this.subscriptionMiddleware(request, result, noop, noop);
     return result;
   }
 
   public destroy() {
-    // this.subscriptionMiddleware.destroy();
+    this.subscriptionMiddleware.destroy();
   }
 }
 
-// interface SubscriptionMiddleware {
-//   (
-//     req: RequestArguments,
-//     res: SubscriptionResult,
-//     next: JsonRpcEngineNextCallback,
-//     end: JsonRpcEngineEndCallback,
-//   ): Promise<void>;
+interface SubscriptionMiddleware {
+  (
+    req: RequestArguments,
+    res: SubscriptionResult,
+    next: JsonRpcEngineNextCallback,
+    end: JsonRpcEngineEndCallback,
+  ): Promise<void>;
 
-//   destroy(): void;
-// }
+  destroy(): void;
+}
