a432de384ceed5ab1e841acdf46c8d64d8340327
Jake <95890768+cb-jake@users.noreply.github.com>
2023-07-11 16:55:40 -0600

    Remove examples

--- examples/rainbowkit-demo/src/utils.js
+++
@@ -1,8 +0,0 @@
-export const truncateAddress = (address) => {
-  if (!address) return "No Account";
-  const match = address.match(
-    /^(0x[a-zA-Z0-9]{2})[a-zA-Z0-9]+([a-zA-Z0-9]{2})$/
-  );
-  if (!match) return address;
-  return `${match[1]}…${match[2]}`;
-};
