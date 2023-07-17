d51f300a2f057713fbdee84ee82902db61c8a050
Jake <95890768+cb-jake@users.noreply.github.com>
2023-07-17 16:38:38 -0600

    use the SDK

--- apps/testapp/src/pages/index.tsx
+++ apps/testapp/src/pages/index.tsx
@@ -1,16 +1,25 @@
-import { CoinbaseWalletSDK } from '@coinbase/wallet-sdk';
+import { CoinbaseWalletProvider, CoinbaseWalletSDK } from '@coinbase/wallet-sdk';
 import React, { useEffect } from 'react';
 
 export default function Home() {
+  const [sdk, setSdk] = React.useState<CoinbaseWalletSDK | null>(null);
+  const [_, setProvider] = React.useState<CoinbaseWalletProvider | null>(null);
+
   useEffect(() => {
-    const sdk = new CoinbaseWalletSDK({
+    const cbwsdk = new CoinbaseWalletSDK({
       appName: 'Test App',
     });
-    console.log('sdk', sdk);
+    setSdk(cbwsdk);
+    const cbwprovider = cbwsdk.makeWeb3Provider('');
+    console.info('provider', cbwprovider);
+    setProvider(cbwprovider);
   }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>SDK</h1>
+      <pre>{JSON.stringify(sdk, null, 2)}</pre>
+      <h1>Provider</h1>
     </div>
   );
 }
