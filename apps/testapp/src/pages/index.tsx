8292d39bdd32c4d3c32d3cc3c533270fc296ef7a
Jake <95890768+cb-jake@users.noreply.github.com>
2023-07-17 16:04:22 -0600

    add sdk to testapp

---
+++ apps/testapp/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    // const sdk = new CoinbaseWalletSDK();
+  }, []);
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
