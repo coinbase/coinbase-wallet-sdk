05422c73c03d48bbd7464eba2daa490b6699ee74
Jake <95890768+cb-jake@users.noreply.github.com>
2023-07-18 12:38:23 -0600

    dev script working

--- apps/testapp/src/pages/index.tsx
+++ apps/testapp/src/pages/index.tsx
@@ -1,25 +1,33 @@
 import { CoinbaseWalletProvider, CoinbaseWalletSDK } from '@coinbase/wallet-sdk';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 export default function Home() {
-  const [sdk, setSdk] = React.useState<CoinbaseWalletSDK | null>(null);
-  const [_, setProvider] = React.useState<CoinbaseWalletProvider | null>(null);
+  const [_, setSdk] = React.useState<CoinbaseWalletSDK | null>(null);
+  const [provider, setProvider] = React.useState<CoinbaseWalletProvider | null>(null);
 
   useEffect(() => {
     const cbwsdk = new CoinbaseWalletSDK({
       appName: 'Test App',
     });
     setSdk(cbwsdk);
-    const cbwprovider = cbwsdk.makeWeb3Provider('');
+    const cbwprovider = cbwsdk.makeWeb3Provider('http');
     console.info('provider', cbwprovider);
     setProvider(cbwprovider);
   }, []);
 
+  const connect = useCallback(async () => {
+    if (!provider) return;
+    try {
+      await provider.enable();
+    } catch (error) {
+      console.error(error);
+    }
+  }, [provider]);
+
   return (
     <div>
-      <h1>SDK</h1>
-      <pre>{JSON.stringify(sdk, null, 2)}</pre>
-      <h1>Provider</h1>
+      <h1>Connect</h1>
+      <button onClick={connect}>Connect</button>
     </div>
   );
 }
