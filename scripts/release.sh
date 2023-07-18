4a9f348fcdc7790a406911ae250b62a7dc50132f
Jake <95890768+cb-jake@users.noreply.github.com>
2023-07-18 13:00:53 -0600

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,14 +12,14 @@ mainBranch="master"
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ $branch == $mainBranch ]; then
-  echo -e "${PURPLE}Checking all branches are up-to-date..."
+  echo -e "${PURPLE} Checking all branches are up-to-date..."
   echo -e "================================================="
   echo -e " git fetch --all"
   git fetch --all
   echo -e " git pull"
   git pull
   echo -e "-------------------------------------------------"
-  echo -e "${TEAL}Build production and publish..."
+  echo -e "${TEAL} Build production and publish..."
   echo "================================================="
   echo -e "rm -rf ./node_modules"
   rm -rf ./node_modules
@@ -28,7 +28,7 @@ if [ $branch == $mainBranch ]; then
   echo -e "yarn workspace @coinbase/wallet-sdk build:prod"
   yarn workspace @coinbase/wallet-sdk build:prod
   echo "================================================="
-  echo -e " ${GREEN}run 'npm publish'"
+  echo -e " ${GREEN} run 'npm publish'"
   echo "================================================="
 else
   echo -e "${RED}⚠️  Need to publish from ${mainBranch} branch"
@@ -36,8 +36,3 @@ else
   git checkout master
   echo -e "${RED}Run again"
 fi
-
-# TODO: Add Slack notification?
-# - Add release notes generation?
-# - Add version tag as argument?
-# - Add prompt for  npm login?
