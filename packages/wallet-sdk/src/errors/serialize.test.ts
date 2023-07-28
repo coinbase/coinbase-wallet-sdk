8b13d0ac679752b0dc4fa666dc3449f1578ed6fd
Jungho Bang <jungho.bang@coinbase.com>
2023-07-28 16:30:58 -0400

    Fix 4902 (unsupportedChain) error getting wrapped as -32603 & Fork eth-rpc-errors (#954)

--- packages/wallet-sdk/src/errors/serialize.test.ts
+++ packages/wallet-sdk/src/errors/serialize.test.ts
@@ -1,67 +1,9 @@
-import {
-  getErrorCode,
-  serializeError,
-  standardErrorCodes,
-  standardErrorMessage,
-  standardErrors,
-} from './errors';
-import { JSONRPCRequest } from './provider/JSONRPC';
-import { Web3Method } from './relay/Web3Method';
-import { ErrorResponse, isErrorResponse } from './relay/Web3Response';
-
-describe('errors', () => {
-  test('getErrorCode', () => {
-    expect(getErrorCode(4137)).toEqual(4137);
-
-    expect(getErrorCode({ code: 4137 })).toEqual(4137);
-    expect(getErrorCode({ errorCode: 4137 })).toEqual(4137);
-    expect(getErrorCode({ code: 4137, errorCode: 4137 })).toEqual(4137);
-
-    expect(getErrorCode({ code: '4137' })).toEqual(undefined);
-    expect(getErrorCode({ code: undefined })).toEqual(undefined);
-    expect(getErrorCode({ errorCode: '4137' })).toEqual(undefined);
-    expect(getErrorCode({ errorCode: undefined })).toEqual(undefined);
-
-    expect(getErrorCode({})).toEqual(undefined);
-    expect(getErrorCode('4137')).toEqual(undefined);
-    expect(getErrorCode(new Error('generic error'))).toEqual(undefined);
-
-    expect(getErrorCode(null)).toEqual(undefined);
-    expect(getErrorCode(undefined)).toEqual(undefined);
-
-    const errorResponse: ErrorResponse = {
-      method: Web3Method.generic,
-      errorMessage: 'test error message',
-      errorCode: 4137,
-    };
-    expect(isErrorResponse(errorResponse)).toEqual(true);
-    expect(getErrorCode(errorResponse)).toEqual(4137);
-  });
-
-  test('standardErrorMessage', () => {
-    expect(standardErrorMessage(undefined)).toEqual('Unknown error');
-
-    // default error message
-    expect(standardErrorMessage(standardErrorCodes.provider.userRejectedRequest)).toEqual(
-      expect.stringContaining('rejected')
-    );
-
-    // non-standard error code
-    expect(standardErrorMessage(0)).toEqual(
-      'Unspecified error message. This is a bug, please report it.'
-    );
-  });
-
-  test('unsupportedChain error', () => {
-    const errorWithoutChainID = standardErrors.provider.unsupportedChain();
-    expect(errorWithoutChainID.code).toEqual(standardErrorCodes.provider.unsupportedChain);
-    expect(errorWithoutChainID.message).toEqual(expect.stringContaining('Unrecognized chain ID'));
-
-    const errorWithChainID = standardErrors.provider.unsupportedChain(1234);
-    expect(errorWithChainID.code).toEqual(standardErrorCodes.provider.unsupportedChain);
-    expect(errorWithChainID.message).toEqual(expect.stringContaining('Unrecognized chain ID 1234'));
-  });
-});
+import { JSONRPCRequest } from '../provider/JSONRPC';
+import { Web3Method } from '../relay/Web3Method';
+import { ErrorResponse } from '../relay/Web3Response';
+import { standardErrorCodes } from './constants';
+import { standardErrors } from './errors';
+import { serializeError } from './serialize';
 
 describe('serializeError', () => {
   test('with ErrorResponse object', () => {
@@ -89,6 +31,17 @@ describe('serializeError', () => {
     expect(serialized.docUrl).toContain(`code=${standardErrorCodes.provider.userRejectedRequest}`);
   });
 
+  test('with unsupportedChain', () => {
+    const error = standardErrors.provider.unsupportedChain();
+
+    const serialized = serializeError(error);
+    expect(serialized.code).toEqual(standardErrorCodes.provider.unsupportedChain);
+    expect(serialized.message).toEqual(error.message);
+    expect(serialized.stack).toEqual(expect.stringContaining('Unrecognized chain ID'));
+    expect(serialized.docUrl).toMatch(/.*version=\d+\.\d+\.\d+.*/);
+    expect(serialized.docUrl).toContain(`code=${standardErrorCodes.provider.unsupportedChain}`);
+  });
+
   test('with Error object', () => {
     const error = new Error('test Error object');
 
@@ -114,7 +67,7 @@ describe('serializeError', () => {
     const error = { unknown: 'error' };
     const serialized = serializeError(error, 'test_request');
     expect(serialized.code).toEqual(standardErrorCodes.rpc.internal);
-    expect(serialized.message).toEqual('Internal JSON-RPC error.');
+    expect(serialized.message).toEqual('Unspecified error message.');
     expect(serialized.docUrl).toMatch(/.*version=\d+\.\d+\.\d+.*/);
     expect(serialized.docUrl).toContain(`code=${standardErrorCodes.rpc.internal}`);
   });
---
+++ packages/wallet-sdk/src/errors/utils.ts
@@ -0,0 +1,152 @@
+import { errorValues, standardErrorCodes } from './constants';
+
+const FALLBACK_MESSAGE = 'Unspecified error message.';
+
+export const JSON_RPC_SERVER_ERROR_MESSAGE = 'Unspecified server error.';
+
+type ErrorValueKey = keyof typeof errorValues;
+
+/**
+ * Gets the message for a given code, or a fallback message if the code has
+ * no corresponding message.
+ */
+export function getMessageFromCode(
+  code: number | undefined,
+  fallbackMessage: string = FALLBACK_MESSAGE
+): string {
+  if (code && Number.isInteger(code)) {
+    const codeString = code.toString();
+
+    if (hasKey(errorValues, codeString)) {
+      return errorValues[codeString as ErrorValueKey].message;
+    }
+    if (isJsonRpcServerError(code)) {
+      return JSON_RPC_SERVER_ERROR_MESSAGE;
+    }
+  }
+  return fallbackMessage;
+}
+
+/**
+ * Returns whether the given code is valid.
+ * A code is only valid if it has a message.
+ */
+export function isValidCode(code: number): boolean {
+  if (!Number.isInteger(code)) {
+    return false;
+  }
+
+  const codeString = code.toString();
+  if (errorValues[codeString as ErrorValueKey]) {
+    return true;
+  }
+
+  if (isJsonRpcServerError(code)) {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Returns the error code from an error object.
+ */
+export function getErrorCode(error: unknown): number | undefined {
+  if (typeof error === 'number') {
+    return error;
+  } else if (isErrorWithCode(error)) {
+    return error.code ?? error.errorCode;
+  }
+
+  return undefined;
+}
+
+interface ErrorWithCode {
+  code?: number;
+  errorCode?: number;
+}
+
+function isErrorWithCode(error: unknown): error is ErrorWithCode {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    (typeof (error as ErrorWithCode).code === 'number' ||
+      typeof (error as ErrorWithCode).errorCode === 'number')
+  );
+}
+
+/**
+ * Serializes the given error to an Ethereum JSON RPC-compatible error object.
+ * Merely copies the given error's values if it is already compatible.
+ * If the given error is not fully compatible, it will be preserved on the
+ * returned object's data.originalError property.
+ */
+
+export interface SerializedEthereumRpcError {
+  code: number; // must be an integer
+  message: string;
+  data?: unknown;
+  stack?: string;
+}
+
+export function serialize(
+  error: unknown,
+  { shouldIncludeStack = false } = {}
+): SerializedEthereumRpcError {
+  const serialized: Partial<SerializedEthereumRpcError> = {};
+
+  if (
+    error &&
+    typeof error === 'object' &&
+    !Array.isArray(error) &&
+    hasKey(error as Record<string, unknown>, 'code') &&
+    isValidCode((error as SerializedEthereumRpcError).code)
+  ) {
+    const _error = error as Partial<SerializedEthereumRpcError>;
+    serialized.code = _error.code;
+
+    if (_error.message && typeof _error.message === 'string') {
+      serialized.message = _error.message;
+
+      if (hasKey(_error, 'data')) {
+        serialized.data = _error.data;
+      }
+    } else {
+      serialized.message = getMessageFromCode((serialized as SerializedEthereumRpcError).code);
+
+      serialized.data = { originalError: assignOriginalError(error) };
+    }
+  } else {
+    serialized.code = standardErrorCodes.rpc.internal;
+
+    serialized.message = hasStringProperty(error, 'message') ? error.message : FALLBACK_MESSAGE;
+    serialized.data = { originalError: assignOriginalError(error) };
+  }
+
+  if (shouldIncludeStack) {
+    serialized.stack = hasStringProperty(error, 'stack') ? error.stack : undefined;
+  }
+  return serialized as SerializedEthereumRpcError;
+}
+
+// Internal
+
+function isJsonRpcServerError(code: number): boolean {
+  return code >= -32099 && code <= -32000;
+}
+
+function assignOriginalError(error: unknown): unknown {
+  if (error && typeof error === 'object' && !Array.isArray(error)) {
+    return Object.assign({}, error);
+  }
+  return error;
+}
+
+function hasKey(obj: Record<string, unknown>, key: string) {
+  return Object.prototype.hasOwnProperty.call(obj, key);
+}
+
+function hasStringProperty<T>(obj: unknown, prop: keyof T): obj is T {
+  return (
+    typeof obj === 'object' && obj !== null && prop in obj && typeof (obj as T)[prop] === 'string'
+  );
+}
