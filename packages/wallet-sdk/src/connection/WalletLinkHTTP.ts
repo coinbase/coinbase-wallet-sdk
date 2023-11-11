af4182996a5ac81b5113a4632645d3e427a70476
Jungho Bang <jungho.bang@coinbase.com>
2023-11-10 21:05:07 -0800

    swap

--- packages/wallet-sdk/src/connection/WalletLinkHTTP.ts
+++ packages/wallet-sdk/src/connection/WalletLinkHTTP.ts
@@ -8,6 +8,23 @@ export class WalletLinkHTTP {
     private readonly handleIncomingEvent: (_: ServerMessage) => void
   ) {}
 
+  // mark unseen events as seen
+  private markUnseenEventsAsSeen(events: ServerMessageEvent[]) {
+    const credentials = `${this.sessionId}:${this.sessionKey}`;
+    const auth = `Basic ${btoa(credentials)}`;
+
+    Promise.all(
+      events.map((e) =>
+        fetch(`${this.linkAPIUrl}/events/${e.eventId}/seen`, {
+          method: 'POST',
+          headers: {
+            Authorization: auth,
+          },
+        })
+      )
+    ).catch((error) => console.error('Unabled to mark event as failed:', error));
+  }
+
   async fetchUnseenEventsAPI() {
     const credentials = `${this.sessionId}:${this.sessionKey}`;
     const auth = `Basic ${btoa(credentials)}`;
@@ -49,22 +66,5 @@ export class WalletLinkHTTP {
     } else {
       throw new Error(`Check unseen events failed: ${response.status}`);
     }
-  }
-
-  // mark unseen events as seen
-  private markUnseenEventsAsSeen(events: ServerMessageEvent[]) {
-    const credentials = `${this.sessionId}:${this.sessionKey}`;
-    const auth = `Basic ${btoa(credentials)}`;
-
-    Promise.all(
-      events.map((e) =>
-        fetch(`${this.linkAPIUrl}/events/${e.eventId}/seen`, {
-          method: 'POST',
-          headers: {
-            Authorization: auth,
-          },
-        })
-      )
-    ).catch((error) => console.error('Unabled to mark event as failed:', error));
   }
 }
