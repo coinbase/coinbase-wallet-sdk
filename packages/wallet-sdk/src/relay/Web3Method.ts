154ba57364fb90a086167e69f07bd6f5298ceb74
Jungho Bang <jungho.bang@coinbase.com>
2023-11-14 17:51:49 -0800

    lfg

--- packages/wallet-sdk/src/relay/Web3Method.ts
+++ packages/wallet-sdk/src/relay/Web3Method.ts
@@ -1,19 +1,18 @@
 // Copyright (c) 2018-2023 Coinbase, Inc. <https://www.coinbase.com/>
 // Licensed under the Apache License, version 2.0
 
-export enum Web3Method {
-  requestEthereumAccounts = 'requestEthereumAccounts',
-  signEthereumMessage = 'signEthereumMessage',
-  signEthereumTransaction = 'signEthereumTransaction',
-  submitEthereumTransaction = 'submitEthereumTransaction',
-  ethereumAddressFromSignedMessage = 'ethereumAddressFromSignedMessage',
-  scanQRCode = 'scanQRCode',
-  generic = 'generic',
-  childRequestEthereumAccounts = 'childRequestEthereumAccounts',
-  addEthereumChain = 'addEthereumChain',
-  switchEthereumChain = 'switchEthereumChain',
-  makeEthereumJSONRPCRequest = 'makeEthereumJSONRPCRequest',
-  watchAsset = 'watchAsset',
-  selectProvider = 'selectProvider',
-  connectAndSignIn = 'connectAndSignIn',
-}
+export type Web3Method =
+  | 'requestEthereumAccounts'
+  | 'signEthereumMessage'
+  | 'signEthereumTransaction'
+  | 'submitEthereumTransaction'
+  | 'ethereumAddressFromSignedMessage'
+  | 'scanQRCode'
+  | 'generic'
+  | 'childRequestEthereumAccounts'
+  | 'addEthereumChain'
+  | 'switchEthereumChain'
+  | 'makeEthereumJSONRPCRequest'
+  | 'watchAsset'
+  | 'selectProvider'
+  | 'connectAndSignIn';
