5b224286890083c956336610632d2aec69f9e6b5
Felix Zhang <felix.zhang@coinbase.com>
2023-11-15 15:35:36 -0800

    removed dup

--- apps/testapp/src/components/RpcMethods/methods/sendMethods.ts
+++ apps/testapp/src/components/RpcMethods/methods/sendMethods.ts
@@ -39,39 +39,4 @@ const ethSendTransactionLegacy = {
   ],
 };
 
-const ethSendTransaction1559 = {
-  method: 'eth_sendTransaction',
-  params: [
-    { key: 'from', required: true },
-    { key: 'to', required: true },
-    { key: 'value', required: true },
-    { key: 'gasLimit', required: true },
-    { key: 'maxFeePerGas', required: true },
-    { key: 'maxPriorityFeePerGas', required: true },
-  ],
-  format: (data: Record<string, string>) => [
-    {
-      from: data.from,
-      to: data.to,
-      value: data.value,
-      gasLimit: data.gasLimit,
-      maxFeePerGas: data.maxFeePerGas,
-      maxPriorityFeePerGas: data.maxPriorityFeePerGas,
-    },
-  ],
-  shortcuts: [
-    {
-      key: 'Example 1559 Tx',
-      data: {
-        from: ADDR_TO_FILL,
-        to: ADDR_TO_FILL,
-        value: '0x1',
-        gasLimit: '0x5208',
-        maxFeePerGas: '0x2540be400',
-        maxPriorityFeePerGas: '0x3b9aca00',
-      },
-    },
-  ],
-};
-
-export const sendMethods: RpcMethod[] = [ethSendTransactionLegacy, ethSendTransaction1559];
+export const sendMethods: RpcMethod[] = [ethSendTransactionLegacy];
