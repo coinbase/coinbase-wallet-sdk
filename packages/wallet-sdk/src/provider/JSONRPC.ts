a2cfa50ebdc725ad4da8511668ed6acd9b6598af
Jungho Bang <jungho.bang@coinbase.com>
2023-11-20 23:17:16 -0800

    | { method: 'eth_accounts'; params: null; result: AddressString[] }

--- packages/wallet-sdk/src/provider/JSONRPC.ts
+++ packages/wallet-sdk/src/provider/JSONRPC.ts
@@ -5,35 +5,34 @@ import BN from 'bn.js';
 
 import { AddressString, HexString, IntNumber } from '../core/type';
 
-export type JSONRPCMethod = keyof typeof JSONRPCInterfaces;
+export type JSONRPCMethod = JSONRPCInterface['method'];
 
-export type JSONRPCRequest<M extends JSONRPCMethod = JSONRPCMethod> = Extract<
-  _JSONRPCRequest<M>,
-  { method: M }
->;
-
-export type JSONRPCResponse<M extends JSONRPCMethod = JSONRPCMethod> =
-  | Extract<_JSONRPCResponse<M>, { method: M }>
-  | _JSONRPCResponseError;
-
-export function isValidJSONRPCMethod(method: string): method is JSONRPCMethod {
-  return method in JSONRPCInterfaces;
+export function isJSONRPCRequest<M extends JSONRPCMethod>(
+  request: unknown,
+  method: M
+): request is JSONRPCRequest<M> {
+  return (
+    typeof request === 'object' &&
+    request !== null &&
+    (request as JSONRPCRequest<M>).method === method
+  );
 }
 
-type _JSONRPCRequest<M extends JSONRPCMethod> = {
+export type JSONRPCRequest<M extends JSONRPCMethod = JSONRPCMethod> = {
   jsonrpc: '2.0';
   id: number;
   method: M;
-  params: (typeof JSONRPCInterfaces)[M]['params'];
+  params: Extract<JSONRPCInterface, { method: M }>['params'];
 };
 
-type _JSONRPCResponse<M extends JSONRPCMethod> = {
+export type JSONRPCResponse<M extends JSONRPCMethod = JSONRPCMethod> = {
   jsonrpc: '2.0';
   id: number;
-  result: (typeof JSONRPCInterfaces)[M]['result'];
+  result: Extract<JSONRPCInterface, { method: M }>['result'];
 };
 
-type _JSONRPCResponseError = {
+// TODO: revisit if this is still needed
+export type JSONRPCResponseError = {
   jsonrpc: '2.0';
   id: number;
   error: {
@@ -43,143 +42,70 @@ type _JSONRPCResponseError = {
   };
 };
 
-const JSONRPCInterfaces = {
-  eth_accounts: {
-    params: null,
-    result: {} as AddressString[],
-  },
-  eth_coinbase: {
-    params: null,
-    result: {} as AddressString,
-  },
-  net_version: {
-    params: null,
-    result: {} as string,
-  },
-  eth_chainId: {
-    params: null,
-    result: {} as string,
-  },
-  eth_uninstallFilter: {
-    params: {} as [HexString],
-    result: {} as boolean,
-  },
-  eth_requestAccounts: {
-    params: null,
-    result: {} as AddressString[],
-  },
-  eth_sign: {
-    params: {} as [AddressString, Buffer],
-    result: {} as HexString,
-  },
-  eth_ecRecover: {
-    params: {} as [Buffer, Buffer],
-    result: {} as AddressString,
-  },
-  personal_sign: {
-    params: {} as [Buffer, AddressString],
-    result: {} as HexString,
-  },
-  personal_ecRecover: {
-    params: {} as [Buffer, Buffer],
-    result: {} as AddressString,
-  },
-  eth_signTransaction: {
-    params: {} as EthereumTransactionParams,
-    result: {} as HexString,
-  },
-  eth_sendRawTransaction: {
-    params: {} as [Buffer],
-    result: {} as HexString,
-  },
-  eth_sendTransaction: {
-    params: {} as EthereumTransactionParams,
-    result: {} as HexString,
-  },
-  eth_signTypedData_v1: {
-    params: {} as [object, AddressString],
-    result: {} as HexString,
-  },
-  eth_signTypedData_v2: {
-    params: null,
-    result: {} as never,
-  },
-  eth_signTypedData_v3: {
-    params: {} as [AddressString, object],
-    result: {} as HexString,
-  },
-  eth_signTypedData_v4: {
-    params: {} as [AddressString, object],
-    result: {} as HexString,
-  },
-  eth_signTypedData: {
-    params: {} as [AddressString, object],
-    result: {} as HexString,
-  },
-  wallet_addEthereumChain: {
-    params: {} as [
-      {
-        chainId: string;
-        blockExplorerUrls?: string[];
-        chainName?: string;
-        iconUrls?: string[];
-        rpcUrls?: string[];
-        nativeCurrency?: {
-          name: string;
-          symbol: string;
-          decimals: number;
-        };
-      },
-    ],
-    result: null,
-  },
-  wallet_switchEthereumChain: {
-    params: {} as [
-      {
-        chainId: string;
-      },
-    ],
-    result: null,
-  },
-  wallet_watchAsset: {
-    params: {} as WatchAssetParams | [WatchAssetParams],
-    result: {} as boolean,
-  },
-  eth_subscribe: {
-    params: {} as [
-      'newHeads' | 'logs' | 'newPendingTransactions' | 'syncing',
-      {
-        address?: AddressString;
-        topics?: string[];
-      },
-    ],
-    result: {} as unknown,
-  },
-  eth_unsubscribe: {
-    params: {} as string[],
-    result: {} as unknown,
-  },
-  eth_newFilter: {
-    params: {} as unknown,
-    result: {} as HexString,
-  },
-  eth_newBlockFilter: {
-    params: null,
-    result: {} as HexString,
-  },
-  eth_newPendingTransactionFilter: {
-    params: null,
-    result: {} as HexString,
-  },
-  eth_getFilterChanges: {
-    params: {} as [HexString],
-    result: {} as unknown,
-  },
-  eth_getFilterLogs: {
-    params: {} as [HexString],
-    result: {} as unknown,
-  },
-} as const;
+type JSONRPCInterface =
+  | { method: 'eth_accounts'; params: null; result: AddressString[] }
+  | { method: 'eth_coinbase'; params: null; result: AddressString }
+  | { method: 'net_version'; params: null; result: string }
+  | { method: 'eth_chainId'; params: null; result: string }
+  | { method: 'eth_uninstallFilter'; params: [HexString]; result: boolean }
+  | { method: 'eth_requestAccounts'; params: null; result: AddressString[] }
+  | { method: 'eth_sign'; params: [AddressString, Buffer]; result: HexString }
+  | { method: 'eth_ecRecover'; params: [Buffer, Buffer]; result: AddressString }
+  | { method: 'personal_sign'; params: [Buffer, AddressString]; result: HexString }
+  | { method: 'personal_ecRecover'; params: [Buffer, Buffer]; result: AddressString }
+  | { method: 'eth_signTransaction'; params: EthereumTransactionParams; result: HexString }
+  | { method: 'eth_sendRawTransaction'; params: [Buffer]; result: HexString }
+  | { method: 'eth_sendTransaction'; params: EthereumTransactionParams; result: HexString }
+  | { method: 'eth_signTypedData_v1'; params: [object, AddressString]; result: HexString }
+  | { method: 'eth_signTypedData_v2'; params: null; result: never }
+  | { method: 'eth_signTypedData_v3'; params: [AddressString, object]; result: HexString }
+  | { method: 'eth_signTypedData_v4'; params: [AddressString, object]; result: HexString }
+  | { method: 'eth_signTypedData'; params: [AddressString, object]; result: HexString }
+  | {
+      method: 'wallet_addEthereumChain';
+      params: [
+        {
+          chainId: string;
+          blockExplorerUrls?: string[];
+          chainName?: string;
+          iconUrls?: string[];
+          rpcUrls?: string[];
+          nativeCurrency?: {
+            name: string;
+            symbol: string;
+            decimals: number;
+          };
+        },
+      ];
+      result: null;
+    }
+  | {
+      method: 'wallet_switchEthereumChain';
+      params: [
+        {
+          chainId: string;
+        },
+      ];
+      result: null;
+    }
+  | { method: 'wallet_watchAsset'; params: WatchAssetParams | [WatchAssetParams]; result: boolean }
+  | {
+      method: 'eth_subscribe';
+      params: [
+        'newHeads' | 'logs' | 'newPendingTransactions' | 'syncing',
+        {
+          address?: AddressString;
+          topics?: string[];
+        },
+      ];
+      result: unknown;
+    }
+  | { method: 'eth_unsubscribe'; params: string[]; result: unknown }
+  | { method: 'eth_newFilter'; params: unknown; result: HexString }
+  | { method: 'eth_newBlockFilter'; params: null; result: HexString }
+  | { method: 'eth_newPendingTransactionFilter'; params: null; result: HexString }
+  | { method: 'eth_getFilterChanges'; params: [HexString]; result: unknown }
+  | { method: 'eth_getFilterLogs'; params: [HexString]; result: unknown };
 
 type EthereumTransactionParams = {
   fromAddress: AddressString;
