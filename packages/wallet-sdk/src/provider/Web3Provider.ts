09052831730215b2d3768deda8fe6192d7f3da45
Jungho Bang <jungho.bang@coinbase.com>
2023-11-21 00:57:19 -0800

    applying type changes

--- packages/wallet-sdk/src/provider/Web3Provider.ts
+++ packages/wallet-sdk/src/provider/Web3Provider.ts
@@ -2,7 +2,8 @@
 // Licensed under the Apache License, version 2.0
 
 import { Callback } from '../core/type';
-import { JSONRPCRequest } from './JSONRPC';
+import { JSONRPCRequest } from './JSONRPCRequest';
+import { JSONRPCResponse } from './JSONRPCResponse';
 
 export interface Web3Provider {
   send(request: JSONRPCRequest): JSONRPCResponse;
