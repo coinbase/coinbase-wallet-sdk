466c367e58bd806455eeefc0c11fc43a4d83c3f5
Felix Zhang <22125939+fan-zhang-sv@users.noreply.github.com>
2023-11-21 16:36:39 -0800

    Misc update (#1079)
    * web3Methods
    
    * childRequestEthereumAccounts
    
    * save sdk version in localstorage

--- packages/wallet-sdk/src/relay/walletlink/type/Web3Method.ts
+++ packages/wallet-sdk/src/relay/walletlink/type/Web3Method.ts
@@ -1,18 +1,21 @@
 // Copyright (c) 2018-2023 Coinbase, Inc. <https://www.coinbase.com/>
 // Licensed under the Apache License, version 2.0
 
-export type Web3Method =
-  | 'requestEthereumAccounts'
-  | 'signEthereumMessage'
-  | 'signEthereumTransaction'
-  | 'submitEthereumTransaction'
-  | 'ethereumAddressFromSignedMessage'
-  | 'scanQRCode'
-  | 'generic'
-  | 'childRequestEthereumAccounts'
-  | 'addEthereumChain'
-  | 'switchEthereumChain'
-  | 'makeEthereumJSONRPCRequest'
-  | 'watchAsset'
-  | 'selectProvider'
-  | 'connectAndSignIn';
+export const web3Methods = [
+  'requestEthereumAccounts',
+  'signEthereumMessage',
+  'signEthereumTransaction',
+  'submitEthereumTransaction',
+  'ethereumAddressFromSignedMessage',
+  'scanQRCode',
+  'generic',
+  'childRequestEthereumAccounts',
+  'addEthereumChain',
+  'switchEthereumChain',
+  'makeEthereumJSONRPCRequest',
+  'watchAsset',
+  'selectProvider',
+  'connectAndSignIn',
+] as const;
+
+export type Web3Method = (typeof web3Methods)[number];
