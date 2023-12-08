03f418dc8d7aa45fffb2a627caf0b3b93b56b4b0
Jungho Bang <jungho.bang@coinbase.com>
2023-12-08 12:31:18 -0500

    Optimize compile-assets.js (#1094)
    * remove copyright
    
    * optimize compile asset

--- packages/wallet-sdk/compile-assets.js
+++ packages/wallet-sdk/compile-assets.js
@@ -7,18 +7,13 @@ const glob = require('glob');
 const sass = require('sass');
 const { optimize } = require('svgo');
 
-const COPYRIGHT = `
-  // Copyright (c) 2018-2023 Coinbase, Inc. <https://www.coinbase.com/>
-  // Licensed under the Apache License, version 2.0
-`;
-
 async function main() {
   // compile SCSS
   const scssFiles = glob.sync(`${__dirname}/src/**/*.scss`);
   for (const filePath of scssFiles) {
     console.info(`Compiling ${filePath}...`);
     const css = sass.renderSync({ file: filePath, outputStyle: 'compressed' }).css.toString('utf8');
-    const ts = `${COPYRIGHT}\n\nexport default \`${css}\``;
+    const ts = `export default (() => \`${css}\`)();`;
     fs.writeFileSync(filePath.replace(/\.scss$/, '-css.ts'), ts, {
       mode: 0o644,
     });
@@ -36,7 +31,7 @@ async function main() {
       // so we enable multipass for that to happen
       multipass: true,
     });
-    const ts = `${COPYRIGHT}\n\nexport default \`${data}\``;
+    const ts = `export default \`${data}\``;
     fs.writeFileSync(filePath.replace(/\.svg$/, '-svg.ts'), ts, {
       mode: 0o644,
     });
