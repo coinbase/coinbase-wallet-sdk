26665dee8d8564acc074b0f4e5c588290b6ea97c
Nate Reiners <nate.reiners@coinbase.com>
2024-03-26 11:32:51 -0700

    let WLRelayAdapter determine what methods are unsupported

--- packages/wallet-sdk/src/sign/walletlink/WLSigner.ts
+++ packages/wallet-sdk/src/sign/walletlink/WLSigner.ts
@@ -1,7 +1,6 @@
 import { Signer, SignerUpdateListener } from '../SignerInterface';
 import { WLRelayAdapter } from './relay/WLRelayAdapter';
 import { WALLETLINK_URL } from ':core/constants';
-import { standardErrors } from ':core/error';
 import { AddressString } from ':core/type';
 import { RequestArguments } from ':core/type/ProviderInterface';
 
@@ -27,19 +26,7 @@ export class WLSigner implements Signer {
     return await this.request<AddressString[]>({ method: 'eth_requestAccounts' });
   }
 
-  // TODO: add WL support for new RPC methods
-  private isUnsupportedByWL(methodName: string) {
-    return [
-      'wallet_getCapabilities',
-      'wallet_sendTransaction',
-      'wallet_getTransactionStatus',
-    ].includes(methodName);
-  }
-
   async request<T>(requestArgs: RequestArguments): Promise<T> {
-    if (this.isUnsupportedByWL(requestArgs.method)) {
-      Promise.reject(standardErrors.rpc.methodNotSupported());
-    }
     return this.adapter.request<T>(requestArgs);
   }
 
