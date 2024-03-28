3b048f86252d70040d7cd43ec6864c5db9ead94c
Felix Zhang <felix.zhang@coinbase.com>
2024-03-28 13:22:16 -0700

    version selector + drawer

--- apps/testapp/src/components/Layout.tsx
+++ apps/testapp/src/components/Layout.tsx
@@ -3,15 +3,23 @@ import {
   Box,
   Button,
   Container,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
   Heading,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
+  useBreakpointValue,
+  useDisclosure,
 } from '@chakra-ui/react';
+import { useMemo } from 'react';
 
-import { useCBWSDK } from '../context/CBWSDKReactContextProvider';
+import { sdkVersions, useCBWSDK } from '../context/CBWSDKReactContextProvider';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -20,44 +28,100 @@ type LayoutProps = {
 export const WIDTH_2XL = '1536px';
 
 export function Layout({ children }: LayoutProps) {
-  const { provider, smartWalletOnly, setPreference } = useCBWSDK();
+  const { provider, smartWalletOnly, setPreference, sdkVersion, setSDKVersion } = useCBWSDK();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
-  const handleDisconnect = () => {
-    if (provider) {
-      provider.disconnect();
-    }
+  const handleClockDocs = () => {
+    window.open('https://docs.cloud.coinbase.com/wallet-sdk/docs/welcome', '_blank');
   };
+
+  const handleDisconnect = async () => {
+    if (provider) {
+      await provider.disconnect();
+    }
+    localStorage.clear();
+  };
+
+  const configs = useMemo(() => {
+    return (
+      <>
+        <Menu>
+          <MenuButton colorScheme="telegram" as={Button} rightIcon={<ChevronDownIcon />}>
+            {`SDK: ${sdkVersion}`}
+          </MenuButton>
+          <MenuList>
+            {sdkVersions.map((version) => (
+              <MenuItem
+                color={'MenuText'}
+                key={version}
+                icon={version === sdkVersion ? <CheckIcon /> : null}
+                onClick={() => setSDKVersion(version)}
+              >
+                {version}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+        {sdkVersion === '4.0' && (
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              {`smartWalletOnly: ${smartWalletOnly}`}
+            </MenuButton>
+            <MenuList>
+              {[true, false].map((b) => (
+                <MenuItem
+                  color={'MenuText'}
+                  key={b.toString()}
+                  icon={b === smartWalletOnly ? <CheckIcon /> : null}
+                  onClick={() => setPreference(b)}
+                >
+                  {b.toString()}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        )}
+      </>
+    );
+  }, [sdkVersion, smartWalletOnly, setPreference, setSDKVersion]);
 
   return (
     <Box minH="100vh" bg="blackAlpha.100">
       <Box as="header" shadow="lg" py={6} bg="blackAlpha.900" color="whiteAlpha.900">
         <Container maxW={WIDTH_2XL}>
           <Flex justifyContent="space-between" alignItems="center">
-            <Heading>Coinbase Wallet SDK - Playground</Heading>
+            <Heading>Coinbase Wallet SDK</Heading>
             <Flex justifyContent="space-between" alignItems="center" gap={4}>
-              <Menu>
-                <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                  {`smartWalletOnly: ${smartWalletOnly}`}
-                </MenuButton>
-                <MenuList>
-                  {[true, false].map((b) => (
-                    <MenuItem
-                      color={'MenuText'}
-                      key={b.toString()}
-                      icon={b === smartWalletOnly ? <CheckIcon /> : null}
-                      onClick={() => setPreference(b)}
-                    >
-                      {b.toString()}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </Menu>
+              {isMobile ? (
+                <Button colorScheme="telegram" onClick={onOpen}>
+                  Config
+                </Button>
+              ) : (
+                configs
+              )}
+              <Button onClick={handleClockDocs}>Docs</Button>
               {/* TODO: There is an issue where `this` is undefined within the sdk instance. */}
-              <Button onClick={handleDisconnect}>Disconnect</Button>
+              <Button colorScheme="red" onClick={handleDisconnect}>
+                Reset
+              </Button>
             </Flex>
           </Flex>
         </Container>
       </Box>
+      {isMobile && (
+        <Drawer placement="top" onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
+            <DrawerBody>
+              <Flex justifyContent="flex-start" alignItems="center" gap={4} paddingY={2}>
+                {configs}
+              </Flex>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      )}
       <Flex flex={1} as="main" mt={6}>
         {children}
       </Flex>
--- apps/testapp/src/context/CBWSDKReactContextProvider.tsx
+++ apps/testapp/src/context/CBWSDKReactContextProvider.tsx
@@ -1,4 +1,6 @@
-import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
+import { CoinbaseWalletSDK as CoinbaseWalletSDK40 } from '@coinbase/wallet-sdk';
+import { CoinbaseWalletSDK as CoinbaseWalletSDK37 } from '@coinbase/wallet-sdk-3.7';
+import { CoinbaseWalletSDK as CoinbaseWalletSDK39 } from '@coinbase/wallet-sdk-3.9';
 import React, { useEffect, useMemo } from 'react';
 
 type CBWSDKProviderProps = {
@@ -7,11 +9,22 @@ type CBWSDKProviderProps = {
 
 const CBWSDKReactContext = React.createContext(null);
 const SMART_WALLET_ONLY_KEY = 'smart_wallet_only';
+const SELECTED_SDK_KEY = 'selected_sdk_version';
+export const sdkVersions = ['4.0', '3.9', '3.7'] as const;
+export type SDKVersionType = (typeof sdkVersions)[number];
 
 export function CBWSDKReactContextProvider({ children }: CBWSDKProviderProps) {
+  const [version, setVersion] = React.useState<SDKVersionType | undefined>(undefined);
   const [smartWalletOnly, setSmartWalletOnly] = React.useState<boolean | undefined>(undefined);
   const [sdk, setSdk] = React.useState(null);
   const [provider, setProvider] = React.useState(null);
+
+  useEffect(() => {
+    if (version === undefined) {
+      const savedVersion = localStorage.getItem(SELECTED_SDK_KEY) as SDKVersionType;
+      setVersion(sdkVersions.includes(savedVersion) ? (savedVersion as SDKVersionType) : '4.0');
+    }
+  }, [version]);
 
   useEffect(() => {
     if (smartWalletOnly === undefined) {
@@ -21,22 +34,39 @@ export function CBWSDKReactContextProvider({ children }: CBWSDKProviderProps) {
   }, [smartWalletOnly]);
 
   useEffect(() => {
-    const cbwsdk = new CoinbaseWalletSDK({
-      appName: 'SDK Playground',
-      chainIds: [84532, 8452],
-      smartWalletOnly,
-    });
-    setSdk(cbwsdk);
+    let cbwsdk;
+    if (version === '4.0') {
+      cbwsdk = new CoinbaseWalletSDK40({
+        appName: 'SDK Playground',
+        chainIds: [84532, 8452],
+        smartWalletOnly,
+      });
+      setSdk(cbwsdk);
+    } else if (version === '3.9' || version === '3.7') {
+      const SDK = version === '3.9' ? CoinbaseWalletSDK39 : CoinbaseWalletSDK37;
+      cbwsdk = new SDK({
+        appName: 'Test App',
+        enableMobileWalletLink: true,
+      });
+      setSdk(cbwsdk);
+    }
+    if (!cbwsdk) return;
     const cbwprovider = cbwsdk.makeWeb3Provider();
-    setProvider(cbwprovider);
     cbwprovider.on('disconnect', () => {
       location.reload();
     });
-  }, [smartWalletOnly]);
+    window.ethereum = cbwprovider;
+    setProvider(cbwprovider);
+  }, [version, smartWalletOnly]);
 
   const setPreference = (smartWalletOnly: boolean) => {
     localStorage.setItem(SMART_WALLET_ONLY_KEY, smartWalletOnly.toString());
     setSmartWalletOnly(smartWalletOnly);
+  };
+
+  const setSDKVersion = (version: SDKVersionType) => {
+    localStorage.setItem(SELECTED_SDK_KEY, version);
+    setVersion(version);
   };
 
   const ctx = useMemo(
@@ -45,6 +75,8 @@ export function CBWSDKReactContextProvider({ children }: CBWSDKProviderProps) {
       provider,
       smartWalletOnly,
       setPreference,
+      sdkVersion: version,
+      setSDKVersion,
     }),
     [sdk, provider]
   );
