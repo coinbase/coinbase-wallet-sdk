7f92b0f57caa38f0343aedf7b2c5b8de0235bcf3
Jungho Bang <jungho.bang@coinbase.com>
2024-04-17 17:59:24 -0700

    fix test

--- packages/wallet-sdk/src/sign/SignerConfigurator.test.ts
+++ packages/wallet-sdk/src/sign/SignerConfigurator.test.ts
@@ -1,8 +1,8 @@
-import { PopUpCommunicator } from '../core/communicator/PopUpCommunicator';
 import { SCWSigner } from './scw/SCWSigner';
 import { SignerConfigurator } from './SignerConfigurator';
 import { SignRequestHandlerListener } from './UpdateListenerInterface';
 import { WLSigner } from './walletlink/WLSigner';
+import { PopUpCommunicator } from ':core/communicator/PopUpCommunicator';
 
 jest.mock('./scw/transport/PopUpCommunicator');
 
