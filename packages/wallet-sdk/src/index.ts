ba156d1b4ecae28e6e15e21d371910c6ee71bff5
Nate Reiners <nate.reiners@coinbase.com>
2024-04-23 13:11:30 -0700

    export types CoinbaseWalletProvider and ProviderInterface for dev convenience

--- packages/wallet-sdk/src/index.ts
+++ packages/wallet-sdk/src/index.ts
@@ -1,3 +1,5 @@
 // Copyright (c) 2018-2024 Coinbase, Inc. <https://www.coinbase.com/>
 
+export type { CoinbaseWalletProvider } from './CoinbaseWalletProvider';
 export { CoinbaseWalletSDK } from './CoinbaseWalletSDK';
+export type { ProviderInterface } from './core/type/ProviderInterface';
