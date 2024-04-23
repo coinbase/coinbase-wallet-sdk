04717f598b132eeee4e903ecb5137eb1222cd5d4
Jungho Bang <jungho.bang@coinbase.com>
2024-04-22 21:49:38 -0700

    Revert "redefine config messages????"
    This reverts commit 9cd87141359723467ed122a1fa9ca5ecf9afe619.

--- packages/wallet-sdk/src/core/message/ConfigMessage.ts
+++ packages/wallet-sdk/src/core/message/ConfigMessage.ts
@@ -1,40 +1,25 @@
 import { Message, MessageID } from '.';
 
-export type ConfigUpdateMessage = ScopedConfigMessage<'popup'> | ScopedConfigMessage<'signer'>;
+export interface ConfigUpdateMessage extends Message {
+  event: ConfigEvent;
+  data?: unknown;
+}
 
 export function isConfigUpdateMessage(message: Message): message is ConfigUpdateMessage {
-  return (
-    'scope' in message &&
-    'event' in message &&
-    (message.scope === 'popup' || message.scope === 'signer')
-  );
+  return (message as ConfigUpdateMessage).event !== undefined;
 }
 
-export type ConfigResponseMessage = Message & {
+export interface ConfigResponseMessage extends Message {
   requestId: MessageID;
   data?: unknown;
-};
-
-export enum PopupConfigEvent {
-  Loaded = 'PopupLoaded',
-  Unload = 'PopupUnload',
 }
 
-export enum SignerConfigEvent {
+export enum ConfigEvent {
+  PopupLoaded = 'PopupLoaded',
+  PopupUnload = 'PopupUnload',
+
   SelectSignerType = 'selectSignerType',
-  WalletLinkSessionRequest = 'WalletLinkSessionRequest',
   WalletLinkUpdate = 'WalletLinkUpdate',
-}
-
-type EventScopes = {
-  popup: PopupConfigEvent;
-  signer: SignerConfigEvent;
-};
-
-interface ScopedConfigMessage<S extends keyof EventScopes> extends Message {
-  scope: S;
-  event: EventScopes[S];
-  data?: unknown;
 }
 
 export type SignerType = 'scw' | 'walletlink' | 'extension';
