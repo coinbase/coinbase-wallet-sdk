0eb82fb74fa737f43f68aad9c9f7db65873f9c38
Felix Zhang <felix.zhang@coinbase.com>
2024-04-25 16:23:12 -0700

    default

--- packages/wallet-sdk/src/index.ts
+++ packages/wallet-sdk/src/index.ts
@@ -1,4 +1,6 @@
 // Copyright (c) 2018-2024 Coinbase, Inc. <https://www.coinbase.com/>
+import { CoinbaseWalletSDK } from './CoinbaseWalletSDK';
+export default CoinbaseWalletSDK;
 
 export type { CoinbaseWalletProvider } from './CoinbaseWalletProvider';
 export { CoinbaseWalletSDK } from './CoinbaseWalletSDK';
