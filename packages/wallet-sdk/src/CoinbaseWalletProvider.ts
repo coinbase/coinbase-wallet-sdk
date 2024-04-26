c5d7051851c8dc562e8c972860ded5bfe98ac673
Nate Reiners <nate.reiners@coinbase.com>
2024-04-26 16:10:19 -0700

    provide chainId getter

--- packages/wallet-sdk/src/CoinbaseWalletProvider.ts
+++ packages/wallet-sdk/src/CoinbaseWalletProvider.ts
@@ -32,6 +32,10 @@ export class CoinbaseWalletProvider extends EventEmitter implements ProviderInte
     return this.accounts.length > 0;
   }
 
+  public get chainId() {
+    return this.chain.id;
+  }
+
   public async request<T>(args: RequestArguments): Promise<T> {
     const invalidArgsError = checkErrorForInvalidRequestArgs(args);
     if (invalidArgsError) throw invalidArgsError;
