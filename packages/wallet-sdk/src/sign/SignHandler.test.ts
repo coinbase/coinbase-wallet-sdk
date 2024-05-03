1488df04fa340916e30d0b71db15b2a96ce9ede2
Jungho Bang <jungho.bang@coinbase.com>
2024-05-03 09:26:17 -0700

    tests

--- packages/wallet-sdk/src/sign/SignHandler.test.ts
+++ packages/wallet-sdk/src/sign/SignHandler.test.ts
@@ -1,5 +1,6 @@
 import { fetchSignerType } from './SignHandler';
 import { Communicator } from ':core/communicator/Communicator';
+import { Preference } from ':core/provider/interface';
 
 const communicator = new Communicator();
 const mockPostMessage = jest.fn();
@@ -7,15 +8,17 @@ communicator.postMessage = mockPostMessage;
 
 describe('SignerConfigurator', () => {
   describe('handshake', () => {
+    const metadata = { appName: 'Test App', appLogoUrl: null, appChainIds: [1] };
+    const preference: Preference = { options: 'all' };
+
     it('should complete signerType selection correctly', async () => {
       mockPostMessage.mockResolvedValueOnce({
         data: 'scw',
       });
-
       const signerType = await fetchSignerType({
         communicator,
-        preference: { options: 'all' },
-        metadata: { appName: 'Test App', appLogoUrl: null, appChainIds: [1] },
+        preference,
+        metadata,
       });
       expect(signerType).toEqual('scw');
     });
