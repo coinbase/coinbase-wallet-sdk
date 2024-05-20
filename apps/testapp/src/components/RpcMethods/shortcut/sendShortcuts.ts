ed2f510966367a8b439db032db6f1e7ac327ff6c
Nate Reiners <nate.reiners@coinbase.com>
2024-05-20 16:52:16 -0700

    don't send to self - dapps are blocked from calling directly to the smart wallet's address

--- apps/testapp/src/components/RpcMethods/shortcut/sendShortcuts.ts
+++ apps/testapp/src/components/RpcMethods/shortcut/sendShortcuts.ts
@@ -6,7 +6,7 @@ const ethSendTransactionShortcuts: ShortcutType[] = [
     key: 'Example Tx',
     data: {
       from: ADDR_TO_FILL,
-      to: ADDR_TO_FILL,
+      to: '0xd7876e5dbd2da268a35416ba75d0ce154ed949e3',
       value: '0x0',
     },
   },
