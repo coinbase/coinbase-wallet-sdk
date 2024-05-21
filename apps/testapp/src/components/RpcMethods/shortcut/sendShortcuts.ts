d7df2a24cb3e9b23720bdc6405d664307d7b7668
Nate Reiners <nate.reiners@coinbase.com>
2024-05-20 17:14:48 -0700

    use black hole address

--- apps/testapp/src/components/RpcMethods/shortcut/sendShortcuts.ts
+++ apps/testapp/src/components/RpcMethods/shortcut/sendShortcuts.ts
@@ -6,7 +6,7 @@ const ethSendTransactionShortcuts: ShortcutType[] = [
     key: 'Example Tx',
     data: {
       from: ADDR_TO_FILL,
-      to: '0xd7876e5dbd2da268a35416ba75d0ce154ed949e3',
+      to: '0x0000000000000000000000000000000000000000',
       value: '0x0',
     },
   },
