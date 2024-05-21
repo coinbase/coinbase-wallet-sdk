e02b016a2b76f0749ee9beb70bb18fd0b42bc76f
Nate <nate.reiners@coinbase.com>
2024-05-21 11:16:58 -0700

    Fix playground example send: don't send to wallets own address  (#1297)
    * don't send to self - dapps are blocked from calling directly to the smart wallet's address
    
    * use black hole address

--- apps/testapp/src/components/RpcMethods/shortcut/sendShortcuts.ts
+++ apps/testapp/src/components/RpcMethods/shortcut/sendShortcuts.ts
@@ -6,7 +6,7 @@ const ethSendTransactionShortcuts: ShortcutType[] = [
     key: 'Example Tx',
     data: {
       from: ADDR_TO_FILL,
-      to: ADDR_TO_FILL,
+      to: '0x0000000000000000000000000000000000000000',
       value: '0x0',
     },
   },
