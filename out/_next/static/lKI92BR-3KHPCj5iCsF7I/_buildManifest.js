5ae0f37d0044bb405a15c0151ae32025e28ddc9e
Jungho Bang <jungho.bang@coinbase.com>
2024-05-22 16:20:08 -0700

    cleanup

--- out/_next/static/lKI92BR-3KHPCj5iCsF7I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/221-8bf8a0fde81a1d9b.js","static/chunks/pages/index-e071a987a17bb93c.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
