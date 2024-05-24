72ac6aac62d836029e6c136aa2c72ac5e2014942
Jake <95890768+cb-jake@users.noreply.github.com>
2024-05-23 23:32:44 -0600

    export provider

--- packages/wallet-sdk/src/index.ts
+++ packages/wallet-sdk/src/index.ts
@@ -2,6 +2,6 @@
 import { CoinbaseWalletSDK } from './CoinbaseWalletSDK';
 export default CoinbaseWalletSDK;
 
-export type { CoinbaseWalletProvider } from './CoinbaseWalletProvider';
+export { CoinbaseWalletProvider } from './CoinbaseWalletProvider';
 export { CoinbaseWalletSDK } from './CoinbaseWalletSDK';
 export type { AppMetadata, Preference, ProviderInterface } from './core/provider/interface';
