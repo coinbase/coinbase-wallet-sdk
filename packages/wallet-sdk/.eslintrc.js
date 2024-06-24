7451fdce32a28d8dd9ba515e359e4ceb975f76af
Jungho Bang <jungho.bang@coinbase.com>
2024-06-24 16:35:28 -0700

    #trivial repo clean up (#1327)
    * repo clean up
    
    * repo clean up
    
    * cleanup
    
    * ci
    
    * revert nvmrc
    
    * hmmmm
    
    * est/no-disabled-tests'
    
    * remove unused imports plugin
    
    * Revert "remove unused imports plugin"
    
    This reverts commit da1c70f3e787bb7c995bddfd8c2c3c7286378cad.

--- packages/wallet-sdk/.eslintrc.js
+++ packages/wallet-sdk/.eslintrc.js
@@ -1,19 +1,3 @@
 module.exports = {
-  extends: ['preact', "../../.eslintrc.js"],
-  settings: {
-    react: {
-      pragma: "h",
-    },
-  },
-  rules: {
-    'no-useless-constructor': 'off',
-  },
-  overrides: [
-    {
-      files: ["**/*.test.*"],
-      rules: {
-        "@typescript-eslint/no-explicit-any": "off"
-      }
-    }
-  ]
+  extends: ["./.eslintrc.base.js"],
 };
