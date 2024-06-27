dd81af0284cd5c2e3a321118e8e6097525abd0ba
Jungho Bang <jungho.bang@coinbase.com>
2024-06-26 17:30:55 -0700

    fix test

--- packages/wallet-sdk/src/sign/scw/SCWStateManager.test.ts
+++ packages/wallet-sdk/src/sign/scw/SCWStateManager.test.ts
@@ -107,8 +107,8 @@ describe('SCWStateManager', () => {
       it('should call chainUpdatedListener when switching the active chain', () => {
         stateManager.switchChain(7);
         expect(chainUpdatedListener).toHaveBeenCalledWith({
-          chain: { id: 7, rpcUrl: 'https://chain7.com' },
-          source: 'wallet',
+          id: 7,
+          rpcUrl: 'https://chain7.com',
         });
       });
 
@@ -124,8 +124,8 @@ describe('SCWStateManager', () => {
       it('should call chainUpdatedListener when updated available chains include the active chain', () => {
         stateManager.updateAvailableChains({ 1: 'https://chain1.com' });
         expect(chainUpdatedListener).toHaveBeenCalledWith({
-          chain: { id: 1, rpcUrl: 'https://chain1.com' },
-          source: 'wallet',
+          id: 1,
+          rpcUrl: 'https://chain1.com',
         });
       });
       it('should not call chainUpdatedListener when updated available chains does not include the active chain', () => {
