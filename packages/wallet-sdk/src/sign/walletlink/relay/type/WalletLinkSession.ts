f7dd240e27e6d18db2c9891a870e1aa8e0630c6f
v1rtl <hi@v1rtl.site>
2024-07-12 14:56:48 +0300

    fix: use `bytesToHex`

--- packages/wallet-sdk/src/sign/walletlink/relay/type/WalletLinkSession.ts
+++ packages/wallet-sdk/src/sign/walletlink/relay/type/WalletLinkSession.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2018-2023 Coinbase, Inc. <https://www.coinbase.com/>
 
 import { sha256 } from '@noble/hashes/sha256';
+import { bytesToHex } from '@noble/hashes/utils';
 
 import { randomBytesHex } from ':core/type/util';
 import { ScopedLocalStorage } from ':util/ScopedLocalStorage';
@@ -21,9 +22,7 @@ export class WalletLinkSession {
     this._id = id || randomBytesHex(16);
     this._secret = secret || randomBytesHex(32);
 
-    const hash = sha256.create().update(`${this._id}, ${this._secret} WalletLink`).digest();
-
-    this._key = Array.from(hash, (byte) => byte.toString(16).padStart(2, '0')).join('');
+    this._key = bytesToHex(sha256(`${this._id}, ${this._secret} WalletLink`));
 
     this._linked = !!linked;
   }
