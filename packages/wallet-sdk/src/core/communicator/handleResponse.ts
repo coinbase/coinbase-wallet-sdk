a58e41b4de890ffa858e088c6a9f8829700fda82
Vishnu Madhusoodanan <vishnu.madhusoodanan@coinbase.com>
2024-07-31 18:45:00 -0500

    Update handleResponse function signature

--- packages/wallet-sdk/src/core/communicator/handleResponse.ts
+++ packages/wallet-sdk/src/core/communicator/handleResponse.ts
@@ -1,10 +1,11 @@
 import { Communicator } from './Communicator.native';
 import { CB_KEYS_URL, MOBILE_SDK_RESPONSE_PATH } from ':core/constants';
 
-export function handleResponse(responseUrl: string, keysUrl: string = CB_KEYS_URL) {
+export function handleResponse(responseUrl: string, keysUrl: string = CB_KEYS_URL): boolean {
   const pathname = new URL(responseUrl).pathname;
   if (pathname.includes(MOBILE_SDK_RESPONSE_PATH)) {
     const communicator = Communicator.getInstance(keysUrl);
-    communicator.handleResponse(responseUrl);
+    return communicator.handleResponse(responseUrl);
   }
+  return false;
 }
