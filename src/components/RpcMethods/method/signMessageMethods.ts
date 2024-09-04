0dd9394bc06926d302ed437c77658d6ef0b83be4
Alexander Shoykhet <alex.shoykhet@coinbase.com>
2024-09-04 15:19:25 -0700

    Keep track of the switch chain state to make the verify message functionality more robust

--- src/components/RpcMethods/method/signMessageMethods.ts
+++ src/components/RpcMethods/method/signMessageMethods.ts
@@ -1,14 +1,4 @@
-import {
-  MessageTypes,
-  recoverPersonalSignature,
-  recoverTypedSignature,
-  SignTypedDataVersion,
-  TypedDataV1,
-  TypedMessage,
-} from '@metamask/eth-sig-util';
-
-import { createPublicClient, http } from 'viem';
-import { base, mainnet } from 'viem/chains';
+import { Chain, createPublicClient, http, TypedDataDomain } from 'viem';
 
 import { parseMessage } from '../shortcut/ShortcutType';
 import { RpcRequestInput } from './RpcRequestInput';
@@ -77,83 +67,52 @@ export const verifySignMsg = async ({
   from,
   sign,
   message,
+  chain,
 }: {
   method: string;
   from: string;
   sign: string;
   message: unknown;
+  chain: Chain;
 }) => {
   switch (method) {
     case 'personal_sign': {
-      const msg = `0x${Buffer.from(message as string, 'utf8').toString('hex')}`;
-
-      try {
-        const recoveredAddr = recoverPersonalSignature({
-          data: msg,
-          signature: sign,
-        });
-        if (recoveredAddr === from) {
-          return `SigUtil Successfully verified signer as ${recoveredAddr}`;
-        } else {
-          return `SigUtil Failed to verify signer when comparing ${recoveredAddr} to ${from}`;
-        }
-      } catch (e: any) {
-        const client = createPublicClient({
-          chain: base,
-          transport: http(),
-        });
-
-        console.log("ASDFASDf passing in", {
-          address: from as `0x${string}`,
-          message: msg,
-          signature: sign as `0x${string}`,
-        })
-        const valid = await client.verifyMessage({
-          address: from as `0x${string}`,
-          message: message as string,
-          signature: sign as `0x${string}`,
-        })
-        if (valid) {
-          return `SigUtil Successfully verified signer as ${from}`;
-        } else {
-          return `SigUtil Failed to verify signer`;
-        }
-      }
-    }
-    case 'eth_signTypedData_v1': {
-      const recoveredAddr = recoverTypedSignature({
-        data: message as TypedDataV1,
-        signature: sign,
-        version: SignTypedDataVersion.V1,
+      const client = createPublicClient({
+        chain,
+        transport: http(),
       });
-      if (recoveredAddr === from) {
-        return `SigUtil Successfully verified signer as ${recoveredAddr}`;
+
+      const valid = await client.verifyMessage({
+        address: from as `0x${string}`,
+        message: message as string,
+        signature: sign as `0x${string}`,
+      })
+      if (valid) {
+        return `SigUtil Successfully verified signer as ${from}`;
       } else {
-        return `SigUtil Failed to verify signer when comparing ${recoveredAddr} to ${from}`;
+        return `SigUtil Failed to verify signer`;
       }
     }
-    case 'eth_signTypedData_v3': {
-      const recoveredAddr = recoverTypedSignature({
-        data: message as TypedMessage<MessageTypes>,
-        signature: sign,
-        version: SignTypedDataVersion.V3,
-      });
-      if (recoveredAddr === from) {
-        return `SigUtil Successfully verified signer as ${recoveredAddr}`;
-      } else {
-        return `SigUtil Failed to verify signer when comparing ${recoveredAddr} to ${from}`;
-      }
-    }
+    case 'eth_signTypedData_v1':
+    case 'eth_signTypedData_v3':
     case 'eth_signTypedData_v4': {
-      const recoveredAddr = recoverTypedSignature({
-        data: message as TypedMessage<MessageTypes>,
-        signature: sign,
-        version: SignTypedDataVersion.V4,
+      const client = createPublicClient({
+        chain,
+        transport: http(),
       });
-      if (recoveredAddr === from) {
-        return `SigUtil Successfully verified signer as ${recoveredAddr}`;
+
+      const valid = await client.verifyTypedData({
+        address: from as `0x${string}`,
+        domain: message['domain'] as TypedDataDomain,
+        types: message['types'] as any,
+        primaryType: message['primaryType'] as string,
+        message: message['message'] as any,
+        signature: sign as `0x${string}`,
+      })
+      if (valid) {
+        return `SigUtil Successfully verified signer as ${from}`;
       } else {
-        return `SigUtil Failed to verify signer when comparing ${recoveredAddr} to ${from}`;
+        return `SigUtil Failed to verify signer`;
       }
     }
     default:
