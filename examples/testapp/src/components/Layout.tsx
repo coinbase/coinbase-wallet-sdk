fa2b8b567e11e26fcfc2613656dba0cdc8a3b0ae
Jake <95890768+cb-jake@users.noreply.github.com>
2024-09-25 08:54:45 -0600

    update testapp to use sdk latest

--- examples/testapp/src/components/Layout.tsx
+++ examples/testapp/src/components/Layout.tsx
@@ -65,7 +65,7 @@ export function Layout({ children }: LayoutProps) {
             ))}
           </MenuList>
         </Menu>
-        {(sdkVersion === 'HEAD' || sdkVersion === '4.0.4') && (
+        {(sdkVersion === 'HEAD' || sdkVersion === 'Latest') && (
           <>
             <Menu>
               <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
