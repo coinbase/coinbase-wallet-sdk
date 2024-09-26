019fb4b51eb2939858a8e3fe5e3861e9b7bb4843
cb-jake <95890768+cb-jake@users.noreply.github.com>
2024-09-26 16:21:06 -0600

    add support for any config parameter (#1402)
    * extend preferences object for any config
    
    * update the Preference type to support new config values
    
    * remove keysUrl as defined config
    
    * update doc blocks and keysurl
    
    * update docs
    
    * revert keysUrl change

--- packages/wallet-sdk/src/core/provider/interface.ts
+++ packages/wallet-sdk/src/core/provider/interface.ts
@@ -42,10 +42,57 @@ export interface AppMetadata {
   appChainIds: number[];
 }
 
-export interface Preference {
-  options: 'all' | 'smartWalletOnly' | 'eoaOnly';
+type PostOnboardingAction = 'none' | 'onramp' | 'magicspend';
+
+type OnrampPrefillOptions = {
+  contractAddress?: string;
+  amount: string;
+  chainId: number;
+};
+
+export type Preference = {
+  /**
+   * @deprecated internal use only.
+   */
   keysUrl?: string;
-}
+  /**
+   * @param options
+   */
+  options: 'all' | 'smartWalletOnly' | 'eoaOnly';
+  /**
+   * @param postOnboardingAction
+   * @type {PostOnboardingAction}
+   * @description This option only applies to Coinbase Smart Wallet. Displays CTAs to the user based on the preference of the app.
+   * These CTAs are part of prebuilt UI components that are available to the Coinbase
+   * Smart Wallet.
+   *
+   * Possible values:
+   * - `none`: No action is recommended post-onboarding. (Default experience)
+   * - `onramp`: Recommends initiating the onramp flow, allowing users to prefill their account with an optional asset.
+   * - `magicspend`: Suggests linking the users retail Coinbase account for seamless transactions.
+   */
+  postOnboardingAction?: PostOnboardingAction;
+  /**
+   * @param onrampPrefillOptions
+   * @type {OnrampPrefillOptions}
+   * @description This option only applies to Coinbase Smart Wallet. Requires `postOnboardingAction` to be set to `onramp`. When not configured,
+   * The onramp screen defaults to an asset selector with 0 as the initial amount.
+   *
+   * - Prefills the onramp flow with the specified asset, chain, and suggested amount, allowing users to prefill their account.
+   * - Ensure the asset and chain are supported by the onramp provider (e.g., Coinbase Pay - CBPay).
+   *
+   * See https://docs.cdp.coinbase.com/onramp/docs/layer2#available-assets for a list of supported assets and networks.
+   */
+  onrampPrefillOptions?: OnrampPrefillOptions;
+  /**
+   * @param attributionDataSuffix
+   * @type {Hex}
+   * @note Smart Wallet only
+   * @description This option only applies to Coinbase Smart Wallet. Data suffix to be appended to the initCode or executeBatch calldata
+   * Coinbase Smart Wallet expects a 4 byte hex string. If the suffix is not a 4 byte hex string, the Smart Wallet will not apply the data suffix.
+   */
+  attributionDataSuffix?: string;
+} & Record<string, unknown>;
 
 export interface ConstructorOptions {
   metadata: AppMetadata;
