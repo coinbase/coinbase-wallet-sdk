6a4534f60146fbce5676b1e90b6d1dbfcf0129aa
Jake <95890768+cb-jake@users.noreply.github.com>
2024-10-01 13:04:55 -0600

    disable headers

--- examples/testapp/next.config.js
+++ examples/testapp/next.config.js
@@ -2,17 +2,17 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "unsafe-none", // Or 'same-origin' to test COOP errors
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       headers: [
+  //         {
+  //           key: "Cross-Origin-Opener-Policy",
+  //           value: "unsafe-none", // Or 'same-origin' to test COOP errors
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
