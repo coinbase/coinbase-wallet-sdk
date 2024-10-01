ffe2d71c3dc58a9dbb037ee2d45e823ce0301cd9
Jake <95890768+cb-jake@users.noreply.github.com>
2024-10-01 13:12:24 -0600

    remove next config headers

--- examples/testapp/next.config.js
+++ examples/testapp/next.config.js
@@ -1,18 +1,4 @@
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       headers: [
-  //         {
-  //           key: "Cross-Origin-Opener-Policy",
-  //           value: "unsafe-none", // Or 'same-origin' to test COOP errors
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
-};
+module.exports = {};
