df81d47084ac20883f9e39acbf7ca14fc7ef1660
cb-jake <95890768+cb-jake@users.noreply.github.com>
2024-10-10 13:50:23 -0600

    merge sdk config demo into main page (#1425)
    * merge sdk config into main page
    
    * remove unused var
    
    * update config ui
    
    * lint fixes

--- examples/testapp/src/components/SDKConfig/SDKConfig.tsx
+++ examples/testapp/src/components/SDKConfig/SDKConfig.tsx
@@ -1,62 +1,31 @@
 import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
   Box,
-  Button,
   Card,
   CardBody,
-  CardHeader,
   Code,
   Flex,
   FormControl,
-  FormLabel,
-  FormHelperText,
   Heading,
   Input,
   Text,
-  VStack,
   Switch,
 } from "@chakra-ui/react";
 import React, { useCallback, useMemo, useState } from "react";
-import { createCoinbaseWalletSDK } from "@coinbase/wallet-sdk";
 import { useCBWSDK } from "../../context/CBWSDKReactContextProvider";
 import { Preference } from "@coinbase/wallet-sdk/dist/core/provider/interface";
 import { keccak256, slice, toHex } from "viem";
-import { CreateCoinbaseWalletSDKOptions } from "@coinbase/wallet-sdk/dist/createCoinbaseWalletSDK";
 
 function is0xString(value: string): value is `0x${string}` {
   return value.startsWith("0x");
 }
 
+function computeDataSuffix(value: string): string {
+  return slice(keccak256(toHex(value)), 0, 16);
+}
+
 export function SDKConfig() {
-  const { option, scwUrl } = useCBWSDK();
-  const [config, setConfig] = React.useState<Preference>({
-    options: option,
-    attribution: {
-      auto: true,
-    },
-  });
-
-  const options: CreateCoinbaseWalletSDKOptions = useMemo(() => {
-    const preference: Preference = {
-      options: option,
-      keysUrl: scwUrl,
-      ...config,
-    };
-    return {
-      appName: "SDK Playground",
-      appLogoUrl: null,
-      appChainIds: [84532, 8452],
-      preference,
-    };
-  }, [config, option, scwUrl]);
-
-  const startOnboarding = useCallback(async () => {
-    const sdk = createCoinbaseWalletSDK(options);
-    const provider = sdk.getProvider();
-    await provider.request({ method: "eth_requestAccounts" });
-  }, [options]);
+  const { config, setConfig } = useCBWSDK();
+  const [dataSuffix, setDataSuffix] = useState<string>();
 
   const handleSetAttributionAuto = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -74,21 +43,19 @@ export function SDKConfig() {
   const handleSetDataSuffix = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
       const value = event.target.value;
-      if (is0xString(value)) {
-        setConfig((prev) => ({
-          ...prev,
-          attribution: {
-            dataSuffix: value,
-          },
-        }));
-      }
+      setDataSuffix(value);
+      setConfig((prev) => ({
+        ...prev,
+        attribution: {
+          dataSuffix: computeDataSuffix(value),
+        },
+      }));
     },
     []
   );
 
-  const [dataSuffix, setDataSuffix] = useState("Coinbase Wallet");
   const sixteenByteHex = useMemo(
-    () => slice(keccak256(toHex(dataSuffix)), 0, 16),
+    () => (dataSuffix ? computeDataSuffix(dataSuffix) : undefined),
     [dataSuffix]
   );
 
@@ -98,27 +65,15 @@ export function SDKConfig() {
 
   return (
     <Card>
-      <CardHeader>
-        <Alert status="error">
-          <AlertIcon />
-          <AlertTitle>
-            This section ONLY works for testing onboarding config
-          </AlertTitle>
-        </Alert>
-        <Alert status="info" mt={2}>
-          <AlertIcon />
-          <AlertTitle>
-            Please signout of your Smart Wallet and reset the playground before
-            testing
-          </AlertTitle>
-        </Alert>
-      </CardHeader>
       <CardBody>
-        <Flex justify="space-between" align="center">
-          <Box>
-            <Heading size="md">Attribution</Heading>
-            <Code mt={2}>attribution.auto</Code>
-          </Box>
+        <Heading mt={2} size="md">
+          Attribution
+        </Heading>
+        <Flex justify="space-between" align="center" mt={4}>
+          <Flex alignItems="center">
+            <Heading size="sm">Auto</Heading>
+            <Code ml={2}>attribution.auto</Code>
+          </Flex>
           <Box>
             <FormControl mt={2}>
               <Switch
@@ -141,44 +96,33 @@ export function SDKConfig() {
               md: "row",
             }}
           >
-            <Box>
-              <Heading size="sm">Data Suffix</Heading>
-              <Text fontSize="sm">
+            <Box flexBasis="50%">
+              <Flex alignItems="center">
+                <Heading size="sm">Data Suffix</Heading>
+                <Code ml={2}>attribution.dataSuffix</Code>
+              </Flex>
+              <Text mt={2} fontSize="sm">
                 First 16 bytes of a unique string to identify your onchain
-                activity
+                activity. Update the text box below to have your data suffix
+                applied
               </Text>
               <FormControl mt={2}>
-                <FormLabel>
-                  <Code>attribution.dataSuffix</Code>
-                </FormLabel>
                 <Input
                   mt={2}
                   type="text"
-                  placeholder="Enter String"
-                  onChange={(e) => setDataSuffix(e.target.value)}
-                  value={dataSuffix}
+                  placeholder="Enter string to hash"
+                  onChange={handleSetDataSuffix}
                 />
-                <FormHelperText>
-                  Convert any string into a 16 byte data suffix
-                </FormHelperText>
               </FormControl>
+            </Box>
+            <Box flexBasis="50%" textAlign="right">
               <Code mt={2} colorScheme="telegram">
                 {sixteenByteHex}
               </Code>
             </Box>
-            <VStack>
-              <Input
-                mt={2}
-                placeholder="Data Suffix (16 bytes)"
-                onChange={handleSetDataSuffix}
-              />
-            </VStack>
           </Flex>
         )}
       </CardBody>
-      <Button size="lg" colorScheme="telegram" onClick={startOnboarding}>
-        Start Onboarding
-      </Button>
     </Card>
   );
 }
