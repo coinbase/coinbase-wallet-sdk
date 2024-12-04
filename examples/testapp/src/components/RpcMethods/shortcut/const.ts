7af758fd7d7f6d002a27d28bc71d3b9ad68b4325
Cody Crozier <wccrozier12@gmail.com>
2024-12-04 14:34:27 -0700

    Dynamically update chainid for send calls requests (#1455)

--- examples/testapp/src/components/RpcMethods/shortcut/const.ts
+++ examples/testapp/src/components/RpcMethods/shortcut/const.ts
@@ -1,3 +1,4 @@
 export const EXAMPLE_MESSAGE = `Example Message`;
 
 export const ADDR_TO_FILL = 'ADDR_TO_FILL';
+export const CHAIN_ID_TO_FILL = 'CHAIN_ID_TO_FILL';
--- examples/testapp/src/components/RpcMethods/shortcut/walletTxShortcuts.ts
+++ examples/testapp/src/components/RpcMethods/shortcut/walletTxShortcuts.ts
@@ -1,11 +1,11 @@
-import { ADDR_TO_FILL } from './const';
+import { ADDR_TO_FILL, CHAIN_ID_TO_FILL } from './const';
 import { ShortcutType } from './ShortcutType';
 
 const walletSendCallsShortcuts: ShortcutType[] = [
   {
     key: 'wallet_sendCalls',
     data: {
-      chainId: '84532',
+      chainId: CHAIN_ID_TO_FILL,
       from: ADDR_TO_FILL,
       calls: [],
       version: '1',
