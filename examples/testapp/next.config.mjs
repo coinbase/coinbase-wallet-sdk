a077f9e8e017ecc5f2264fc69de3731704471cb3
Jake <95890768+cb-jake@users.noreply.github.com>
2025-01-31 14:56:48 -0700

    add page extensions

--- examples/testapp/next.config.mjs
+++ examples/testapp/next.config.mjs
@@ -1,4 +1,5 @@
 export default {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/coinbase-wallet-sdk' : undefined,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.js', 'page.jsx'],
 };
