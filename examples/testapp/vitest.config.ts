7e8377bea347bb7b11e7f4f065285e4dd7fade16
eric-ships <ericliu121187@gmail.com>
2025-02-07 09:17:19 -0800

    remove unnecessary

--- examples/testapp/vitest.config.ts
+++ examples/testapp/vitest.config.ts
@@ -1,12 +1,7 @@
-import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    alias: {
-      ':core': path.resolve(__dirname, 'src/core'),
-      ':util': path.resolve(__dirname, 'src/util'),
-    },
     environment: 'jsdom',
     globals: true,
   },
