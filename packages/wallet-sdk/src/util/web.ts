3111eb4c4288b4d68017b0850ba7b8e9feec545f
Arjun Dureja <arjun.dureja@coinbase.com>
2025-02-26 11:43:54 -0700

    Share retry path

--- packages/wallet-sdk/src/util/web.ts
+++ packages/wallet-sdk/src/util/web.ts
@@ -2,6 +2,7 @@ import { NAME, VERSION } from '../sdk-info.js';
 import { getCrossOriginOpenerPolicy } from './checkCrossOriginOpenerPolicy.js';
 import { standardErrors } from ':core/error/errors.js';
 import { Snackbar } from ':sign/walletlink/relay/ui/components/Snackbar/Snackbar.js';
+import { RETRY_SVG_PATH } from ':sign/walletlink/relay/ui/WalletLinkRelayUI.js';
 
 const POPUP_WIDTH = 420;
 const POPUP_HEIGHT = 540;
@@ -11,7 +12,7 @@ const RETRY_BUTTON = {
   info: 'Retry',
   svgWidth: '10',
   svgHeight: '11',
-  path: 'M5.00008 0.96875C6.73133 0.96875 8.23758 1.94375 9.00008 3.375L10.0001 2.375V5.5H9.53133H7.96883H6.87508L7.80633 4.56875C7.41258 3.3875 6.31258 2.53125 5.00008 2.53125C3.76258 2.53125 2.70633 3.2875 2.25633 4.36875L0.812576 3.76875C1.50008 2.125 3.11258 0.96875 5.00008 0.96875ZM2.19375 6.43125C2.5875 7.6125 3.6875 8.46875 5 8.46875C6.2375 8.46875 7.29375 7.7125 7.74375 6.63125L9.1875 7.23125C8.5 8.875 6.8875 10.0312 5 10.0312C3.26875 10.0312 1.7625 9.05625 1 7.625L0 8.625V5.5H0.46875H2.03125H3.125L2.19375 6.43125Z',
+  path: RETRY_SVG_PATH,
   defaultFillRule: 'evenodd',
   defaultClipRule: 'evenodd',
 } as const;
