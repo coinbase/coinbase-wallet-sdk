572c16a71aafda644e1175d92e725ea4a852d0b1
AU_gdev_19 <64915515+Dargon789@users.noreply.github.com>
2025-04-07 05:41:58 +0700

    fix  web site provider bug reload

---
+++ middleware.js
@@ -0,0 +1,11 @@
+import { get } from '@vercel/edge-config';
+import { NextResponse } from 'next/server';
+
+export const config = { matcher: '/welcome' };
+
+export async function middleware() {
+  const greeting = await get('greeting');
+  // NextResponse.json requires at least Next v13.1 or
+  // enabling experimental.allowMiddlewareResponseBody in next.config.js
+  return NextResponse.json(greeting);
+}
